3cf946ea20bc6088edcb4452192ac84e71c07fb6
Balázs Dukai <balazs.dukai@gmail.com>
2023-06-26 10:51:54 +0200

    Add download link and sha256 to validation

--- src/bag3d_pipeline/assets/export/validate_files.py
+++ src/bag3d_pipeline/assets/export/validate_files.py
@@ -7,23 +7,24 @@ from concurrent.futures import ProcessPoolExecutor
 
 
 def cityjson(dirpath: Path, file_id: str, planarity_n_tol: float,
-             planarity_d2p_tol: float) -> dict:
+             planarity_d2p_tol: float, url_root: str, version: str) -> dict:
     results = {
         "cj_zip_ok": None,
         "cj_nr_features": None,
         "cj_nr_invalid": None,
         "cj_all_errors": None,
         "cj_schema_valid": None,
-        "cj_schema_warnings": None
+        "cj_schema_warnings": None,
+        "cj_download": None,
+        "cj_sha256": None
     }
+    inputzipfile = dirpath.joinpath(file_id).with_suffix(".city.json.gz")
     inputfile = dirpath / f"{file_id}.city.json"
-    # temp fix for messup
-    inputfile.unlink(missing_ok=True)
 
     # test zip
     try:
         cmd = " ".join([
-            "gunzip", "-t", str(dirpath.joinpath(file_id).with_suffix(".city.json.gz"))
+            "gunzip", "-t", str(inputzipfile)
         ])
         output, returncode = execute_shell_command_silent(shell_command=cmd,
                                                           cwd=str(dirpath))
@@ -32,11 +33,27 @@ def cityjson(dirpath: Path, file_id: str, planarity_n_tol: float,
         inputfile.unlink(missing_ok=True)
         return results
 
+    # download link and sha256
+    try:
+        cmd = " ".join([
+            "sha256sum", str(inputzipfile)
+        ])
+        output, returncode = execute_shell_command_silent(shell_command=cmd,
+                                                          cwd=str(dirpath))
+        sha256 = output.split(" ")[0]
+        results["cj_sha256"] = sha256
+        results["cj_download"] = create_download_link(
+            url_root=url_root, format="cityjson", file_id=file_id, version=version
+        )
+    except Exception:
+        inputfile.unlink(missing_ok=True)
+        return results
+
     # unzip
     try:
         cmd = " ".join([
             "gunzip", "--keep",
-            str(dirpath.joinpath(file_id).with_suffix(".city.json.gz"))
+            str(inputzipfile)
         ])
         execute_shell_command_silent(shell_command=cmd, cwd=str(dirpath))
     except Exception:
@@ -86,14 +103,17 @@ def cityjson(dirpath: Path, file_id: str, planarity_n_tol: float,
     return results
 
 
-def obj(dirpath: Path, file_id: str, planarity_n_tol: float,
-        planarity_d2p_tol: float) -> dict:
+def obj(dirpath: Path, file_id: str, planarity_n_tol: float, planarity_d2p_tol: float,
+        url_root: str, version: str) -> dict:
     results = {
         "obj_zip_ok": None,
         "obj_nr_features": None,
         "obj_nr_invalid": None,
         "obj_all_errors": None,
+        "obj_download": None,
+        "obj_sha256": None
     }
+    inputzipfile = dirpath.joinpath(f"{file_id}-obj.zip")
     inputfiles = [
         dirpath / f"{file_id}-LoD12-3D.obj", dirpath / f"{file_id}-LoD12-3D.obj.mtl",
         dirpath / f"{file_id}-LoD13-3D.obj", dirpath / f"{file_id}-LoD13-3D.obj.mtl",
@@ -103,7 +123,7 @@ def obj(dirpath: Path, file_id: str, planarity_n_tol: float,
     # test zip
     try:
         cmd = " ".join([
-            "unzip", "-t", str(dirpath.joinpath(f"{file_id}-obj.zip"))
+            "unzip", "-t", str(inputzipfile)
         ])
         output, returncode = execute_shell_command_silent(shell_command=cmd,
                                                           cwd=str(dirpath))
@@ -113,10 +133,27 @@ def obj(dirpath: Path, file_id: str, planarity_n_tol: float,
             inputfile.unlink(missing_ok=True)
         return results
 
+    # download link and sha256
+    try:
+        cmd = " ".join([
+            "sha256sum", str(inputzipfile)
+        ])
+        output, returncode = execute_shell_command_silent(shell_command=cmd,
+                                                          cwd=str(dirpath))
+        sha256 = output.split(" ")[0]
+        results["obj_sha256"] = sha256
+        results["obj_download"] = create_download_link(
+            url_root=url_root, format="obj", file_id=file_id, version=version
+        )
+    except Exception:
+        for inputfile in inputfiles:
+            inputfile.unlink(missing_ok=True)
+        return results
+
     # unzip
     try:
         cmd = " ".join([
-            "unzip", "-o", str(dirpath.joinpath(f"{file_id}-obj.zip"))
+            "unzip", "-o", str(inputzipfile)
         ])
         execute_shell_command_silent(shell_command=cmd, cwd=str(dirpath))
     except Exception:
@@ -160,22 +197,39 @@ def obj(dirpath: Path, file_id: str, planarity_n_tol: float,
     return results
 
 
-def gpkg(dirpath: Path, file_id: str) -> dict:
+def gpkg(dirpath: Path, file_id: str, url_root: str, version: str) -> dict:
     results = {
         "gpkg_zip_ok": None,
         "gpkg_ok": None,
-        "gpkg_nr_features": None
+        "gpkg_nr_features": None,
+        "gpkg_sha256": None,
+        "gpkg_download": None
     }
-    inputzip = dirpath.joinpath(file_id).with_suffix(".gpkg.gz")
+    inputzipfile = dirpath.joinpath(file_id).with_suffix(".gpkg.gz")
 
     # test zip
     try:
         cmd = " ".join([
-            "gunzip", "-t", str(inputzip)
+            "gunzip", "-t", str(inputzipfile)
         ])
         output, returncode = execute_shell_command_silent(shell_command=cmd,
                                                           cwd=str(dirpath))
         results["gpkg_zip_ok"] = True if len(output) == 0 else False
+    except Exception:
+        return results
+
+    # download link and sha256
+    try:
+        cmd = " ".join([
+            "sha256sum", str(inputzipfile)
+        ])
+        output, returncode = execute_shell_command_silent(shell_command=cmd,
+                                                          cwd=str(dirpath))
+        sha256 = output.split(" ")[0]
+        results["gpkg_sha256"] = sha256
+        results["gpkg_download"] = create_download_link(
+            url_root=url_root, format="gpkg", file_id=file_id, version=version
+        )
     except Exception:
         return results
 
@@ -186,7 +240,7 @@ def gpkg(dirpath: Path, file_id: str) -> dict:
             cmd = " ".join([
                 "LD_LIBRARY_PATH=/opt/lib:$LD_LIBRARY_PATH",
                 "/opt/bin/ogrinfo",
-                f"/vsigzip/{inputzip}", layer
+                f"/vsigzip/{inputzipfile}", layer
             ])
             output, returncode = execute_shell_command_silent(shell_command=cmd,
                                                               cwd=str(dirpath))
@@ -204,30 +258,53 @@ def gpkg(dirpath: Path, file_id: str) -> dict:
     return results
 
 
+def create_download_link(url_root: str, format: str, file_id: str, version: str) -> str:
+    tile_id = file_id.replace("-", "/")
+    if format == "cityjson":
+        filename = f"{file_id}.city.json"
+        l = f"{url_root}/{format}/{version}/tiles/{tile_id}/{filename}"
+    elif format == "gpkg":
+        filename = f"{file_id}.gpkg"
+        l = f"{url_root}/{format}/{version}/tiles/{tile_id}/{filename}"
+    elif format == "obj":
+        filename = f"{file_id}-obj.zip"
+        l = f"{url_root}/{format}/{version}/tiles/{tile_id}/{filename}"
+    else:
+        raise ValueError(f"only cityjson, obj, gpkg format is allowed, got {format}")
+    return l
+
+
 def check_formats(input) -> dict:
-    dirpath, tile_id = input
+    dirpath, tile_id, url_root, version = input
     file_id = tile_id.replace("/", "-")
     cj_results = cityjson(dirpath, file_id, planarity_n_tol=20.0,
-                          planarity_d2p_tol=0.001)
-    obj_results = obj(dirpath, file_id, planarity_n_tol=20.0, planarity_d2p_tol=0.001)
-    gpkg_results = gpkg(dirpath, file_id)
+                          planarity_d2p_tol=0.001, url_root=url_root, version=version)
+    obj_results = obj(dirpath, file_id, planarity_n_tol=20.0, planarity_d2p_tol=0.001,
+                      url_root=url_root, version=version)
+    gpkg_results = gpkg(dirpath, file_id, url_root=url_root, version=version)
     return {"tile": tile_id, **cj_results, **obj_results, **gpkg_results}
 
 
 if __name__ == "__main__":
     export_dir = Path("/data/3DBAG/export")
+    url_root = "https://data.3dbag.nl"
+    version = "v20230622"
     with open("/data/3DBAG/export/export_index.csv", "r") as fo:
         csvreader = csv.reader(fo)
         h = next(csvreader)
-        tileids = [(export_dir.joinpath("tiles", row[0]), row[0]) for row in csvreader]
+        tileids = [(export_dir.joinpath("tiles", row[0]), row[0], url_root, version)
+                   for row in csvreader]
 
     fo = export_dir.joinpath("check_all_formats.csv").open("w")
     csvwriter = csv.DictWriter(
         fo, quoting=csv.QUOTE_NONNUMERIC,
-        fieldnames=['tile', 'cj_zip_ok', 'cj_nr_features', 'cj_nr_invalid',
-                    'cj_all_errors', 'cj_schema_valid', 'cj_schema_warnings',
-                    'obj_zip_ok', 'obj_nr_features', 'obj_nr_invalid', 'obj_all_errors',
-                    'gpkg_zip_ok', 'gpkg_ok', 'gpkg_nr_features'])
+        fieldnames=["tile", "cj_zip_ok", "cj_nr_features", "cj_nr_invalid",
+                    "cj_all_errors", "cj_schema_valid", "cj_schema_warnings",
+                    "cj_download", "cj_sha256",
+                    "obj_zip_ok", "obj_nr_features", "obj_nr_invalid", "obj_all_errors",
+                    "obj_download", "obj_sha256",
+                    "gpkg_zip_ok", "gpkg_ok", "gpkg_nr_features",
+                    "gpkg_download", "gpkg_sha256", ])
     csvwriter.writeheader()
 
     try:
