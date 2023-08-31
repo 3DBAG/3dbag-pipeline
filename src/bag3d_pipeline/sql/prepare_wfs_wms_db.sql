8b9521b2fdd7de19c1e4f58be9e632b8bc48d6cb
Gina Stavropoulou <ginastavropoulou@gmail.com>
2023-08-31 14:54:00 +0200

    mat views

--- src/bag3d_pipeline/sql/prepare_wfs_wms_db.sql
+++ src/bag3d_pipeline/sql/prepare_wfs_wms_db.sql
@@ -1,89 +1,154 @@
-CREATE INDEX lod12_2d_geom_idx ON { schema }.lod12_2d USING gist (geom);
 
-CREATE INDEX lod13_2d_geom_idx ON { schema }.lod13_2d USING gist (geom);
+CREATE INDEX lod12_2d_id_idx ON ${lod12_table} USING btree (identificatie);
 
-CREATE INDEX lod13_2d_geom_idx ON { schema }.lod22_2d USING gist (geom);
+CREATE INDEX lod13_2d_id_idx ON ${lod13_table} USING btree (identificatie);
 
-ALTER TABLE
-    { schema }.lod12_2d
-ADD
-    COLUMN dak_type VARCHAR;
+CREATE INDEX lod22_2d_id_idx ON ${lod22_table} USING btree (identificatie);
 
-UPDATE
-    { schema }.lod12_2d ld
-SET
-    dak_type = p.b3_dak_type
+CREATE INDEX pand_id_idx ON ${pand_table} USING btree (identificatie);
+
+DROP MATERIALIZED VIEW IF EXISTS ${mat_view_lod12};
+CREATE materialized VIEW ${mat_view_lod12} AS
+SELECT
+    ld.*,
+    p.b3_dak_type,
+    p.b3_h_maaiveld,
+    p.b3_kas_warenhuis,
+    p.b3_mutatie_ahn3_ahn4,
+    p.b3_nodata_fractie_ahn3,
+    p.b3_nodata_fractie_ahn4,
+    p.b3_nodata_radius_ahn3,
+    p.b3_nodata_radius_ahn4,
+    p.b3_puntdichtheid_ahn3,
+    p.b3_puntdichtheid_ahn4,
+    p.b3_pw_bron,
+    p.b3_pw_datum,
+    p.b3_pw_selectie_reden,
+    p.b3_reconstructie_onvolledig,
+    p.b3_rmse_lod12,
+    p.b3_rmse_lod13,
+    p.b3_rmse_lod22,
+    p.b3_val3dity_lod12,
+    p.b3_val3dity_lod13,
+    p.b3_val3dity_lod22,
+    p.b3_volume_lod12,
+    p.b3_volume_lod13,
+    p.b3_volume_lod22,
+    p.begingeldigheid,
+    p.documentdatum,
+    p.documentnummer,
+    p.eindgeldigheid,
+    p.eindregistratie,
+    p.geconstateerd,
+    p.oorspronkelijkbouwjaar,
+    p.status,
+    p.tijdstipeindregistratielv,
+    p.tijdstipinactief,
+    p.tijdstipinactieflv,
+    p.tijdstipnietbaglv,
+    p.tijdstipregistratie,
+    p.tijdstipregistratielv,
+    p.voorkomenidentificatie
 FROM
-    { schema }.pand p
-WHERE
-    ld.identificatie = p.identificatie;
+    ${lod12_table} ld
+    JOIN ${pand_table} p 
+    ON p.identificatie = ld.identificatie;
 
-ALTER TABLE
-    { schema }.lod12_2d
-ADD
-    COLUMN h_maaiveld FLOAT8;
-
-UPDATE
-    { schema }.lod12_2d ld
-SET
-    h_maaiveld = p.b3_h_maaiveld
+DROP MATERIALIZED VIEW IF EXISTS ${mat_view_lod13};
+CREATE materialized VIEW ${mat_view_lod13} AS
+SELECT
+    ld.*,
+    p.b3_dak_type,
+    p.b3_h_maaiveld,
+    p.b3_kas_warenhuis,
+    p.b3_mutatie_ahn3_ahn4,
+    p.b3_nodata_fractie_ahn3,
+    p.b3_nodata_fractie_ahn4,
+    p.b3_nodata_radius_ahn3,
+    p.b3_nodata_radius_ahn4,
+    p.b3_puntdichtheid_ahn3,
+    p.b3_puntdichtheid_ahn4,
+    p.b3_pw_bron,
+    p.b3_pw_datum,
+    p.b3_pw_selectie_reden,
+    p.b3_reconstructie_onvolledig,
+    p.b3_rmse_lod12,
+    p.b3_rmse_lod13,
+    p.b3_rmse_lod22,
+    p.b3_val3dity_lod12,
+    p.b3_val3dity_lod13,
+    p.b3_val3dity_lod22,
+    p.b3_volume_lod12,
+    p.b3_volume_lod13,
+    p.b3_volume_lod22,
+    p.begingeldigheid,
+    p.documentdatum,
+    p.documentnummer,
+    p.eindgeldigheid,
+    p.eindregistratie,
+    p.geconstateerd,
+    p.oorspronkelijkbouwjaar,
+    p.status,
+    p.tijdstipeindregistratielv,
+    p.tijdstipinactief,
+    p.tijdstipinactieflv,
+    p.tijdstipnietbaglv,
+    p.tijdstipregistratie,
+    p.tijdstipregistratielv,
+    p.voorkomenidentificatie
 FROM
-    { schema }.pand p
-WHERE
-    ld.identificatie = p.identificatie;
+    ${lod13_table} ld
+    JOIN ${pand_table} p 
+    ON p.identificatie = ld.identificatie;
 
-ALTER TABLE
-    { schema }.lod13_2d
-ADD
-    COLUMN dak_type VARCHAR;
-
-UPDATE
-    { schema }.lod13_2d ld
-SET
-    dak_type = p.b3_dak_type
+DROP MATERIALIZED VIEW IF EXISTS ${mat_view_lod22};
+CREATE materialized VIEW ${mat_view_lod22} AS
+SELECT
+    ld.*,
+    p.b3_dak_type,
+    p.b3_h_maaiveld,
+    p.b3_kas_warenhuis,
+    p.b3_mutatie_ahn3_ahn4,
+    p.b3_nodata_fractie_ahn3,
+    p.b3_nodata_fractie_ahn4,
+    p.b3_nodata_radius_ahn3,
+    p.b3_nodata_radius_ahn4,
+    p.b3_puntdichtheid_ahn3,
+    p.b3_puntdichtheid_ahn4,
+    p.b3_pw_bron,
+    p.b3_pw_datum,
+    p.b3_pw_selectie_reden,
+    p.b3_reconstructie_onvolledig,
+    p.b3_rmse_lod12,
+    p.b3_rmse_lod13,
+    p.b3_rmse_lod22,
+    p.b3_val3dity_lod12,
+    p.b3_val3dity_lod13,
+    p.b3_val3dity_lod22,
+    p.b3_volume_lod12,
+    p.b3_volume_lod13,
+    p.b3_volume_lod22,
+    p.begingeldigheid,
+    p.documentdatum,
+    p.documentnummer,
+    p.eindgeldigheid,
+    p.eindregistratie,
+    p.geconstateerd,
+    p.oorspronkelijkbouwjaar,
+    p.status,
+    p.tijdstipeindregistratielv,
+    p.tijdstipinactief,
+    p.tijdstipinactieflv,
+    p.tijdstipnietbaglv,
+    p.tijdstipregistratie,
+    p.tijdstipregistratielv,
+    p.voorkomenidentificatie
 FROM
-    { schema }.pand p
-WHERE
-    ld.identificatie = p.identificatie;
+    ${lod22_table} ld
+    JOIN ${pand_table} p 
+    ON p.identificatie = ld.identificatie;
 
-ALTER TABLE
-    { schema }.lod13_2d
-ADD
-    COLUMN h_maaiveld FLOAT8;
 
-UPDATE
-    { schema }.lod13_2d ld
-SET
-    h_maaiveld = p.b3_h_maaiveld
-FROM
-    { schema }.pand p
-WHERE
-    ld.identificatie = p.identificatie;
-
-ALTER TABLE
-    { schema }.lod22_2d
-ADD
-    COLUMN dak_type VARCHAR;
-
-UPDATE
-    { schema }.lod22_2d ld
-SET
-    dak_type = p.b3_dak_type
-FROM
-    { schema }.pand p
-WHERE
-    ld.identificatie = p.identificatie;
-
-ALTER TABLE
-    { schema }.lod22_2d
-ADD
-    COLUMN h_maaiveld FLOAT8;
-
-UPDATE
-    { schema }.lod22_2d ld
-SET
-    h_maaiveld = p.b3_h_maaiveld
-FROM
-    { schema }.pand p
-WHERE
-    ld.identificatie = p.identificatie;
+CREATE INDEX lod12_geom_idx ON ${mat_view_lod12} USING gist (geom);
+CREATE INDEX lod13_geom_idx ON ${mat_view_lod13} USING gist (geom);
+CREATE INDEX lod13_geom_idx ON ${mat_view_lod22} USING gist (geom);
