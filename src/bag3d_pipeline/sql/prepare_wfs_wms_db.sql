3d914c82e41f24ba8501fc790646e172f41bf391
Gina Stavropoulou <ginastavropoulou@gmail.com>
2023-09-08 10:48:36 +0200

    changed mat view to table

--- src/bag3d_pipeline/sql/prepare_wfs_wms_db.sql
+++ src/bag3d_pipeline/sql/prepare_wfs_wms_db.sql
@@ -1,14 +1,14 @@
 
-CREATE INDEX lod12_2d_id_idx ON ${lod12_table} USING btree (identificatie);
+CREATE INDEX lod12_2d_id_idx ON ${lod12_2d_tmp} USING btree (identificatie);
 
-CREATE INDEX lod13_2d_id_idx ON ${lod13_table} USING btree (identificatie);
+CREATE INDEX lod13_2d_id_idx ON ${lod13_2d_tmp} USING btree (identificatie);
 
-CREATE INDEX lod22_2d_id_idx ON ${lod22_table} USING btree (identificatie);
+CREATE INDEX lod22_2d_id_idx ON ${lod22_2d_tmp} USING btree (identificatie);
 
 CREATE INDEX pand_id_idx ON ${pand_table} USING btree (identificatie);
 
-DROP MATERIALIZED VIEW IF EXISTS ${mat_view_lod12};
-CREATE materialized VIEW ${mat_view_lod12} AS
+DROP TABLE IF EXISTS ${lod12_2d};
+CREATE TABLE ${lod12_2d} AS
 SELECT
     ld.*,
     p.b3_dak_type,
@@ -50,12 +50,12 @@ SELECT
     p.tijdstipregistratielv,
     p.voorkomenidentificatie
 FROM
-    ${lod12_table} ld
+    ${lod12_2d_tmp} ld
     JOIN ${pand_table} p 
     ON p.identificatie = ld.identificatie;
 
-DROP MATERIALIZED VIEW IF EXISTS ${mat_view_lod13};
-CREATE materialized VIEW ${mat_view_lod13} AS
+DROP TABLE IF EXISTS ${lod13_2d};
+CREATE TABLE ${lod13_2d} AS
 SELECT
     ld.*,
     p.b3_dak_type,
@@ -97,12 +97,12 @@ SELECT
     p.tijdstipregistratielv,
     p.voorkomenidentificatie
 FROM
-    ${lod13_table} ld
+    ${lod13_2d_tmp} ld
     JOIN ${pand_table} p 
     ON p.identificatie = ld.identificatie;
 
-DROP MATERIALIZED VIEW IF EXISTS ${mat_view_lod22};
-CREATE materialized VIEW ${mat_view_lod22} AS
+DROP TABLE IF EXISTS ${lod22_2d};
+CREATE TABLE ${lod22_2d} AS
 SELECT
     ld.*,
     p.b3_dak_type,
@@ -144,11 +144,11 @@ SELECT
     p.tijdstipregistratielv,
     p.voorkomenidentificatie
 FROM
-    ${lod22_table} ld
+    ${lod22_2d_tmp} ld
     JOIN ${pand_table} p 
     ON p.identificatie = ld.identificatie;
 
 
-CREATE INDEX lod12_geom_idx ON ${mat_view_lod12} USING gist (geom);
-CREATE INDEX lod13_geom_idx ON ${mat_view_lod13} USING gist (geom);
-CREATE INDEX lod13_geom_idx ON ${mat_view_lod22} USING gist (geom);
+CREATE INDEX lod12_geom_idx ON ${lod12_2d} USING gist (geom);
+CREATE INDEX lod13_geom_idx ON ${lod13_2d} USING gist (geom);
+CREATE INDEX lod13_geom_idx ON ${lod22_2d} USING gist (geom);
