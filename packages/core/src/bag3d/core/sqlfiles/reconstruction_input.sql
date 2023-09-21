3b21b5039b5a2fa4efde351e4c0c8f06ce56a279
Balázs Dukai <balazs.dukai@gmail.com>
2023-09-21 10:43:52 +0200

    Fix column name

--- packages/core/src/bag3d/core/sqlfiles/reconstruction_input.sql
+++ packages/core/src/bag3d/core/sqlfiles/reconstruction_input.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS ${new_table} CASCADE;
 
 CREATE TABLE ${new_table} AS
-WITH bag_kas AS (SELECT bag.*
-                      , bkw.kas_warenhuis
-                 FROM ${bag_cleaned} bag
-                          LEFT JOIN ${bag_kas_warenhuis} bkw USING (fid)
-                          LEFT JOIN ${bag_bag_overlap} bbo USING (fid)
-                 )
+WITH extra_attributes AS (SELECT bag.*
+                               , bkw.kas_warenhuis
+                               , bbo.b3_bag_bag_overlap
+                          FROM ${bag_cleaned} bag
+                                   LEFT JOIN ${bag_kas_warenhuis} bkw USING (fid)
+                                   LEFT JOIN ${bag_bag_overlap} bbo USING (fid))
    , duplicates AS (SELECT *
                          , ROW_NUMBER() OVER (PARTITION BY identificatie) rn
-                    FROM bag_kas)
+                    FROM extra_attributes)
 SELECT fid
      , oorspronkelijkbouwjaar
      , identificatie
