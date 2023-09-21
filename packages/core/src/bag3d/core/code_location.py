2c186a9c62242e1c115b1ee5d813faa429ed36c5
Balázs Dukai <balazs.dukai@gmail.com>
2023-09-21 17:11:25 +0200

    Clean up

--- packages/core/src/bag3d/core/code_location.py
+++ packages/core/src/bag3d/core/code_location.py
@@ -7,9 +7,7 @@ from bag3d.core.jobs import (job_source_input, job_clean_containers,
                              job_ahn3, job_ahn4, job_zuid_holland_reconstruct,
                              job_zuid_holland_export, job_zuid_holland_deploy,
                              job_zuid_holland_export_deploy,
-                             job_nl_export_deploy,
-                             job_nl_reconstruct, job_nl_export, job_nl_deploy,
-                             job_nl_reconstruct_rerun )
+                             job_nl_reconstruct, job_nl_export, job_nl_deploy,)
 
 all_assets = [
     *source_assets,
@@ -31,8 +29,6 @@ all_jobs = [
     job_nl_reconstruct,
     job_nl_export,
     job_nl_deploy,
-    job_nl_reconstruct_rerun,
-    job_nl_export_deploy,
 ]
 
 defs = Definitions(
