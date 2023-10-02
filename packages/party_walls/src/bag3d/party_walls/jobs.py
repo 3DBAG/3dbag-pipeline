f995a51896418ec71a40fe057d572c1fcf771261
Balázs Dukai <balazs.dukai@gmail.com>
2023-10-02 19:19:51 +0200

    Remove non-partitioned assets from party walls job

--- packages/party_walls/src/bag3d/party_walls/jobs.py
+++ packages/party_walls/src/bag3d/party_walls/jobs.py
@@ -10,9 +10,7 @@ job_nl_party_walls = define_asset_job(
     writes CityJSONFeature files. The new features need to be run through *tyler* again, 
     to generate the tiles for export.
     """,
-    selection=AssetSelection.keys(["party_walls", "features_file_index"]) |
-              AssetSelection.keys(["party_walls", "distribution_tiles_files_index"]) |
-              AssetSelection.keys(["party_walls", "party_walls_nl"]) |
+    selection=AssetSelection.keys(["party_walls", "party_walls_nl"]) |
               AssetSelection.keys(
                   ["party_walls", "cityjsonfeatures_with_party_walls_nl"]),
     partitions_def=PartitionDefinition3DBagDistribution()
