0a8ac07c4a13bf6d1a608833aed30826c128e2fe
Gina Stavropoulou <ginastavropoulou@gmail.com>
2024-02-12 22:02:25 +0100

    wrong asset prefix

--- packages/floors_estimation/src/bag3d/floors_estimation/jobs.py
+++ packages/floors_estimation/src/bag3d/floors_estimation/jobs.py
@@ -4,9 +4,9 @@ job_floors_estimation = define_asset_job(
     name="floors_estimation",
     description="""Estimate the number of floors per building and write the
     results back to the CityJSONFeatures.""",
-    selection=AssetSelection.keys(["floor_count_estimation", 
+    selection=AssetSelection.keys(["floors_estimation",
                                    "extract_building_features"]) |
               AssetSelection.keys(
-                  ["floor_count_estimation",
+                  ["floors_estimation",
                    "create_building_features_table"])
 )
