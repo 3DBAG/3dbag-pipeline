e1040b6815abedd35f52f14df62edbab7afce2d4
Gina Stavropoulou <ginastavropoulou@gmail.com>
2024-02-14 13:34:45 +0100

    jobs

--- packages/floors_estimation/src/bag3d/floors_estimation/jobs.py
+++ packages/floors_estimation/src/bag3d/floors_estimation/jobs.py
@@ -4,7 +4,8 @@ job_floors_estimation = define_asset_job(
     name="floors_estimation",
     description="""Estimate the number of floors per building and write the
     results back to the CityJSONFeatures.""",
-    selection=AssetSelection.keys(["floors_estimation", "extract_external_features"])
-    | AssetSelection.keys(["floors_estimation", "extract_3DBAG_features"])
-    | AssetSelection.keys(["floors_estimation", "create_building_features_table"]),
+    selection=AssetSelection.keys(["floors_estimation", "features_file_index"])
+    | AssetSelection.keys(["floors_estimation", "external_features"])
+    | AssetSelection.keys(["floors_estimation", "bag3d_features"])
+    | AssetSelection.keys(["floors_estimation", "all_features"]),
 )
