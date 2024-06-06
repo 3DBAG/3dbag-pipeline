a2aeaa8717b1305c454208ac0e1246e8593ab20c
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-06-06 17:22:31 +0200

    removed dagster config and updated link to data.

--- docker/postgres/entrypoint.sh
+++ docker/postgres/entrypoint.sh
@@ -2,8 +2,6 @@
 pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DB} --no-owner /var/lib/postgresql/data/baseregisters.tar
 psql -d postgres -U ${POSTGRES_USER} -c "create database baseregisters_old;"
 pg_restore -U ${POSTGRES_USER} -d baseregisters_old --no-owner /var/lib/postgresql/data/baseregisters_old.tar
-psql -d postgres -U ${POSTGRES_USER} -c "create database ${DAGSTER_PG_DB};"
-psql -d ${DAGSTER_PG_DB} -U ${POSTGRES_USER} -c "CREATE USER ${DAGSTER_PG_USER} WITH PASSWORD '${DAGSTER_PG_PASSWORD}';"
 psql -d postgres -U ${POSTGRES_USER} -c "create database baseregisters_empty;"
 psql -d baseregisters_empty -c "create extension postgis;"
 
