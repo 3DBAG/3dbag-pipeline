f0607f1364f96432807d40caa24e0d869971bb63
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-06-12 13:29:56 +0200

    database tests

--- packages/common/tests/conftest.py
+++ packages/common/tests/conftest.py
@@ -1,18 +1,43 @@
-from time import sleep
-from pathlib import Path
+import os
 import string
+from pathlib import Path
 from random import choice
 from shutil import rmtree
-import os
 
 import pytest
+from bag3d.common.resources.database import DatabaseConnection
+from psycopg.sql import SQL, Identifier
+from pytest_postgresql import factories
+
 import docker
-from docker import errors
 
-from bag3d.common.resources.database import (DatabaseConnection, db_connection,
-                                             container)
+LOCAL_DIR = os.getenv('PATH_TO_TEST_DATA')
 
-RES_CONTAINER_ID = "pytest-3dbag-pipeline-db_connection"
+HOST = "localhost"
+PORT = os.getenv('POSTGRES_PORT')
+USER = os.getenv('POSTGRES_USER')
+PASSWORD = os.getenv('POSTGRES_PASSWORD')
+DB_NAME = os.getenv('POSTGRES_DB')
+postgresql_noproc = factories.postgresql_noproc(
+    host=HOST,
+    port=PORT,
+    user=USER,
+    password=PASSWORD,
+)
+# Create the postgresql fixture with a new db - if you want to use existing remove dbname parameter
+postgresql = factories.postgresql('postgresql_noproc')
+
+
+@pytest.fixture
+def database(postgresql):
+    db = DatabaseConnection(user=USER, password=PASSWORD,
+                             host=HOST, port=PORT, dbname=DB_NAME)
+
+    yield db
+    query = SQL("""
+            DROP SCHEMA IF EXISTS {schema} CASCADE;
+        """).format(schema = Identifier("test"))
+    db.send_query(query)
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -20,76 +45,9 @@ def setenv():
     os.environ["DAGSTER_DEPLOYMENT"] = "pytest"
 
 
-@pytest.fixture(scope="session", autouse=False)
-def database():
-    """Database for testing, including test data. Runs in a docker container."""
-    image_name = "balazsdukai/3dbag-sample-data:latest"
-    container_name = "pytest-3dbag-pipeline"
-    port = 5565
-
-    # temp dir that is mounted on the container for data
-    r = ''.join(choice(string.ascii_letters) for _ in range(8))
-    tmp = Path(f"/tmp/pytest-3dbag-{r}")
-    tmp.mkdir()
-    tmp.chmod(mode=0o777)
-
-    # set up
-    client = docker.from_env()
-    try:
-        image = client.images.get(image_name)
-    except errors.ImageNotFound:
-        image = client.images.pull(image_name)
-    try:
-        cont = client.containers.run(
-            image, name=container_name, detach=True, volumes=[f"{tmp}:/data", ],
-            ports={"5432/tcp": port})
-    except errors.DockerException as e:
-        try:
-            cont = client.containers.get(container_name)
-        except errors.ContextNotFound:
-            pytest.fail(f"Could not start docker container from {image_name}:\n{e}")
-        else:
-            cont.remove(force=True)
-            pytest.fail(f"Could not start docker container from {image_name}:\n{e}")
-
-    # check setup
-    print(f"Mounted {image_name}/data onto {tmp}")
-    print(f"{tmp} contents:")
-    for i in tmp.iterdir():
-        print(i)
-
-    # run tests, but wait for database to start first
-    sleep(8)
-    db = DatabaseConnection(user="db3dbag_user", password="db3dbag_1234",
-                            host="localhost", port=port, dbname="baseregisters")
-    yield tmp, db
-
-    # teardown
-    cont.remove(force=True, v=True)
-    rmtree(str(tmp))
-    print(f"Deleted docker container {container_name}")
-    print(f"Deleted local directory {tmp}")
-
-
 @pytest.fixture(scope="session")
-def resource_container():
-    return container.configured({"id": RES_CONTAINER_ID})
-
-
-@pytest.fixture(scope="function")
-def resource_db_connection_docker(resource_container):
-    yield db_connection.configured({
-        "docker": {
-            "image_id": "balazsdukai/3dbag-sample-data:latest",
-        },
-        "port": 5564,
-        "user": "db3dbag_user",
-        "password": "db3dbag_1234",
-        "dbname": "baseregisters"
-    })
-    client = docker.from_env()
-    cont = client.containers.get(RES_CONTAINER_ID)
-    cont.remove(force=True, v=True)
+def test_data_dir():
+    yield Path(LOCAL_DIR)
 
 
 @pytest.fixture(scope="session")
@@ -107,3 +65,12 @@ def wkt_testarea():
 def docker_gdal_image():
     """The GDAL docker image to use for the tests"""
     return "ghcr.io/osgeo/gdal:alpine-small-latest"
+
+@pytest.fixture(scope="function")
+def temp_file_store():
+    r = ''.join(choice(string.ascii_letters) for _ in range(8))
+    tmp = Path(f"/tmp/tmp_3dbag_test_{r}")
+    tmp.mkdir()
+    tmp.chmod(mode=0o777)
+    yield tmp
+    rmtree(str(tmp))
--- packages/common/tests/test_database.py
+++ packages/common/tests/test_database.py
@@ -1,12 +1,110 @@
+from bag3d.common.utils.database import (create_schema, drop_table, load_sql,
+                                         postgrestable_from_query,
+                                         postgrestable_metadata, summary_md,
+                                         table_exists)
+from dagster import build_op_context
 from pgutils import PostgresTableIdentifier
+from psycopg.sql import SQL, Identifier
 
-from bag3d.common.utils.database import load_sql
+# def test_load_sql():
+#     query_params = {
+#         'tbl': PostgresTableIdentifier('myschema', 'mytable'),
+#     }
+#     query = load_sql(filename="test_table2.sql", query_params=query_params)
+#     expect = "Composed([SQL('create table table2 as select * from '), Identifier('myschema', 'mytable'), SQL(';')])"
+#     assert str(query) == expect
 
 
-def test_load_sql():
+def test_table_exists(database):
+    context = build_op_context(resources={"db_connection": database})
+    existing_table = PostgresTableIdentifier("lvbag", "pandactueelbestaand")
+
+    assert table_exists(context, existing_table) is True
+
+    non_existing_table = PostgresTableIdentifier("lvbag", "table")
+
+    assert table_exists(context, non_existing_table) is False
+
+
+def test_drop_table(database):
+    context = build_op_context(resources={"db_connection": database})
+    tbl = PostgresTableIdentifier("public", "table1")
     query_params = {
-        'tbl': PostgresTableIdentifier('myschema', 'mytable'),
+        "table1": tbl,
     }
-    query = load_sql(filename="test_table2.sql", query_params=query_params)
-    expect = "Composed([SQL('create table table2 as select * from '), Identifier('myschema', 'mytable'), SQL(';')])"
-    assert str(query) == expect
+
+    query = SQL(
+        """CREATE TABLE {table} (id INTEGER, value TEXT);
+                   INSERT INTO {table} VALUES (1, 'bla');
+                   INSERT INTO {table} VALUES (2, 'foo');"""
+    ).format(table=Identifier(tbl.schema.str, tbl.table.str))
+
+    metadata = postgrestable_from_query(context, query, tbl)
+    assert metadata["Rows"] == 2
+
+    assert table_exists(context, tbl) is True
+
+    drop_table(context, tbl)
+
+    assert table_exists(context, tbl) is False
+
+
+def test_create_schema(database):
+    context = build_op_context(resources={"db_connection": database})
+    schema_name = "test"
+    create_schema(context, schema_name)
+
+    query = SQL(
+        """SELECT count(schema_name)
+                FROM information_schema.schemata 
+                WHERE schema_name = {schema};"""
+    ).format(schema=schema_name)
+    res = context.resources.db_connection.get_dict(query)
+    assert res[0]["count"] == 1
+
+
+def test_summary_md(database):
+    existing_table = PostgresTableIdentifier("lvbag", "pandactueelbestaand")
+
+    null_count = database.count_nulls(existing_table)
+    fields = database.get_fields(existing_table)
+
+    res = summary_md(fields, null_count)
+    assert type(res) == str
+    lines = res.splitlines()
+    assert lines[0] == "| column | type | NULLs |"
+
+
+def test_postgrestable_metadata(database):
+    context = build_op_context(resources={"db_connection": database})
+    table = PostgresTableIdentifier("lvbag", "pandactueelbestaand")
+
+    res = postgrestable_metadata(context, table)
+
+    print(res)
+    assert (
+        res["Database.Schema.Table"] == "baseregisters_test.lvbag.pandactueelbestaand"
+    )
+    assert res["Rows"] == 0
+
+
+def test_postgrestable_from_query(database):
+    context = build_op_context(resources={"db_connection": database})
+    tbl = PostgresTableIdentifier("public", "table1")
+    query_params = {
+        "table1": tbl,
+    }
+
+    query = SQL(
+        """CREATE TABLE {table} (id INTEGER, value TEXT);
+                   INSERT INTO {table} VALUES (1, 'bla');
+                   INSERT INTO {table} VALUES (2, 'foo');"""
+    ).format(table=Identifier(tbl.schema.str, tbl.table.str))
+
+    metadata = postgrestable_from_query(context, query, tbl)
+    assert metadata["Rows"] == 2
+    assert table_exists(context, tbl) is True
+
+    drop_table(context, tbl)
+
+    assert table_exists(context, tbl) is False
