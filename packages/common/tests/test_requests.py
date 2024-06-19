bc95847fb46914254b2d0b48eb455bb5581f24ce
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-06-19 10:36:42 +0200

    request tests

--- packages/common/tests/test_requests.py
+++ packages/common/tests/test_requests.py
@@ -1,6 +1,5 @@
+from bag3d.common.utils.requests import get_extract_download_link, get_metadata
 from pytest import mark
-
-from bag3d.common.utils.requests import get_metadata, get_extract_download_link
 
 
 def test_get_metadata():
@@ -8,6 +7,7 @@ def test_get_metadata():
     assert res
 
 
+@mark.slow
 @mark.parametrize("geofilter", ("testarea", None), ids=["testarea", "NL"])
 def test_download_link(wkt_testarea, geofilter):
     """Can we get a valid download link with a WKT geofilter and also with a None,
@@ -16,8 +16,10 @@ def test_download_link(wkt_testarea, geofilter):
         geofilter = wkt_testarea
     res = get_extract_download_link(
         url="https://api.pdok.nl/brt/top10nl/download/v1_0/full/custom",
-        featuretypes=["gebouw", ],
+        featuretypes=[
+            "gebouw",
+        ],
         data_format="gml",
-        geofilter=geofilter
+        geofilter=geofilter,
     )
     assert res
