b7c557c4cf44c24004021b7e5b4f4f2a80931a62
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-06-19 11:24:24 +0200

    context in core test

--- packages/core/tests/test_assets_input.py
+++ packages/core/tests/test_assets_input.py
@@ -4,18 +4,16 @@ from bag3d.core.assets.input import intermediary
 from dagster import build_op_context
 
 
-def test_bag_kas_warenhuis(database):
+def test_bag_kas_warenhuis(baseregisters_context):
     """Does the complete asset work?"""
-    context = build_op_context(
-        resources={"db_connection": database}
-    )
+
     bag_pandactueelbestaand = PostgresTableIdentifier("lvbag", "pandactueelbestaand")
     top10nl_gebouw = PostgresTableIdentifier("top10nl", "gebouw")
 
     new_table = PostgresTableIdentifier('reconstruction_input', "bag_kas_warenhuis")
 
-    res = intermediary.bag_kas_warenhuis(context, bag_pandactueelbestaand,
+    res = intermediary.bag_kas_warenhuis(baseregisters_context, bag_pandactueelbestaand,
                                          top10nl_gebouw)
     assert isinstance(res.value, PostgresTableIdentifier)
     assert  str(res.value) == f'{new_table.schema}.{new_table.table}'
-    drop_table(context, new_table)
+    drop_table(baseregisters_context, new_table)
--- packages/core/tests/test_assets_top10nl.py
+++ packages/core/tests/test_assets_top10nl.py
@@ -4,18 +4,8 @@ from bag3d.core.assets.top10nl import download
 
 
 @pytest.mark.slow
-def test_extract_top10nl(docker_gdal_image, wkt_testarea):
+def test_extract_top10nl(context):
     """Does the complete asset work?"""
-    context = build_op_context(
-        op_config={
-            "geofilter": wkt_testarea,
-            "featuretypes": ["gebouw", ]
-        },
-        resources={
-            "gdal": gdal.configured({"docker": {"image": docker_gdal_image}}),
-            "file_store": file_store.configured({})
-        }
-    )
     res = download.extract_top10nl(context)
     assert res.value.exists()
     context.resources.file_store.rm(force=True)
