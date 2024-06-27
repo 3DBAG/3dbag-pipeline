4e5387f66cef80ea4215ef00ad1bd132e1141108
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-06-27 13:10:28 +0200

    party walls tests WIP

--- packages/party_walls/tests/conftest.py
+++ packages/party_walls/tests/conftest.py
@@ -2,13 +2,33 @@ from pathlib import Path
 
 import pytest
 
-from bag3d.common.utils.files import bag3d_export_dir
+from bag3d.common.resources.database import container
+import os
+
+import pytest
+from bag3d.common.resources.files import file_store
+from bag3d.common.resources.database import DatabaseConnection
+from dagster import build_op_context
+
+
+
+LOCAL_DIR = os.getenv("PATH_TO_TEST_DATA")
+HOST = "localhost"
+PORT = os.getenv("POSTGRES_PORT")
+USER = os.getenv("POSTGRES_USER")
+PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
 
 
 @pytest.fixture(scope="session")
-def root_data_dir() -> Path:
+def test_data_dir():
+    yield Path(LOCAL_DIR)
+
+
+@pytest.fixture(scope="session")
+def root_data_dir(test_data_dir) -> Path:
     """Root directory path for test data"""
-    return Path("data").resolve()
+    return test_data_dir / "reconstruction_data"
 
 
 @pytest.fixture(scope="session")
@@ -20,7 +40,7 @@ def input_data_dir(root_data_dir) -> Path:
 @pytest.fixture(scope="session")
 def crop_reconstruct_data_dir(input_data_dir) -> Path:
     """Directory for the reconstruction data"""
-    return input_data_dir / "crop_reconstruct"
+    return input_data_dir / "3DBAG" / "crop_reconstruct"
 
 
 @pytest.fixture(scope="session")
@@ -33,3 +53,49 @@ def export_dir_uncompressed(input_data_dir) -> Path:
 def output_data_dir(root_data_dir) -> Path:
     """Directory for data generated during test runs"""
     return root_data_dir / "output"
+
+
+@pytest.fixture(scope="function")
+def database():
+    db = DatabaseConnection(
+        host=HOST, port=PORT, user=USER, password=PASSWORD, dbname=DB_NAME
+    )
+    yield db
+
+
+@pytest.fixture
+def context(database, export_dir_uncompressed, input_data_dir):
+    yield build_op_context(
+        resources={
+            "db_connection": database,
+            "file_store": file_store.configured(
+                 {"data_dir": str(export_dir_uncompressed), }),
+            "file_store_fastssd": file_store.configured(
+                {"data_dir": str(input_data_dir), })}        
+            )
+
+
+@pytest.fixture(scope="session")
+def resource_container():
+    return container.configured({"id": "testid"})
+
+
+# Ref: https://docs.pytest.org/en/latest/example/simple.html#control-skipping-of-tests-according-to-command-line-option
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
--- packages/party_walls/tests/test_party_walls.py
+++ packages/party_walls/tests/test_party_walls.py
@@ -1,78 +1,61 @@
-import pytest
 import os
-from dagster import build_asset_context, materialize_to_memory, RunConfig
-from bag3d.party_walls.assets.party_walls import distribution_tiles_files_index, \
-    party_walls_nl, cityjsonfeatures_with_party_walls_nl, \
-    features_file_index_generator
-from bag3d.common.resources.database import db_connection, container
-from bag3d.common.resources.files import file_store
+
 import pandas as pd
+import pytest
+from bag3d.common.resources.files import file_store
+from bag3d.party_walls.assets.party_walls import (
+    cityjsonfeatures_with_party_walls_nl, distribution_tiles_files_index,
+    features_file_index, features_file_index_generator, party_walls_nl)
+from dagster import (materialize_to_memory)
 
 
-@pytest.fixture(scope="session")
-def resource_container():
-    return container.configured({"id": "testid"})
-
-
-@pytest.fixture(scope="function")
-def resource_db_connection():
-    """db_connection resource"""
-    return db_connection.configured({
-        "host": "localhost",
-        "port": 5432,
-        "user": os.environ.get("PYTEST_DB_USER"),
-        "password": os.environ.get("PYTEST_DB_PW"),
-        "dbname": "baseregisters_test"
-    })
-
-
-@pytest.fixture(scope="session")
-def resource_file_store(output_data_dir):
-    """file_store resource pointing to the test output location"""
-    return file_store.configured({"data_dir": str(output_data_dir), })
-
-
-def test_distribution_tiles_files_index(export_dir_uncompressed):
-    """Can we parse the CityJSON tiles and return valid data?
-    Currently hardcoded for Balázs' local test data.
-    """
+def test_distribution_tiles_files_index(context):
+    """Can we parse the CityJSON tiles and return valid data? """
     tile_ids = ('10/564/624', '10/564/626', '10/566/624', '10/566/626', '9/560/624')
 
     result = distribution_tiles_files_index(
-        context=build_asset_context(
-            resources={"file_store": file_store.configured(
-                {"data_dir": str(export_dir_uncompressed), })}
-        )
+        context
     )
     assert len(result.tree.geometries) == len(tile_ids)
     assert len(result.paths_array) == len(tile_ids)
     result_tile_ids = tuple(sorted(result.export_results.keys()))
     assert result_tile_ids == tile_ids
 
-
-def test_party_walls(resource_db_connection, resource_container,
-                     export_dir_uncompressed, output_data_dir):
+@pytest.mark.slow
+def test_party_walls(database, resource_container,
+                     output_data_dir, export_dir_uncompressed):
     """Can we compute the party walls and other statistics?"""
     result = materialize_to_memory(
-        [distribution_tiles_files_index, party_walls_nl],
+        [party_walls_nl, distribution_tiles_files_index],
         partition_key='10/564/624',
-        resources={"db_connection": resource_db_connection,
-                   "container": resource_container}
+        resources={"db_connection": database,
+                   "container": resource_container,
+                   "file_store": file_store.configured(
+                 {"data_dir": str(export_dir_uncompressed), }),}
     )
     df = result.asset_value("party_walls_nl")
     df.to_csv(str(output_data_dir / "party_walls_nl.csv"))
+    assert not df.empty
     assert result.success
 
 
-def test_features_file_index(crop_reconstruct_data_dir):
+def test_features_file_index_generator(crop_reconstruct_data_dir):
     """Can we find and map all the 5800 citjsonfeature files of the test data?"""
     result = dict(features_file_index_generator(crop_reconstruct_data_dir))
-    assert 5800 == len(result)
+    assert len(result) == 5825
 
 
-def test_cityjsonfeatures_with_party_walls_nl(output_data_dir,
+def test_features_file_index(context):
+    """Can we find and map all the 5800 citjsonfeature files of the test data?"""
+    result = features_file_index(
+         context=context
+    )
+    assert len(result) == 5825
+
+
+def test_cityjsonfeatures_with_party_walls_nl(context,
                                               crop_reconstruct_data_dir,
-                                              resource_file_store):
+                                              output_data_dir):
     """Can we create cityjsonfeatures with the party wall data?
     Currently, this test uses the csv that is created by `test_party_walls`.
     """
@@ -80,10 +63,9 @@ def test_cityjsonfeatures_with_party_walls_nl(output_data_dir,
         features_file_index_generator(crop_reconstruct_data_dir))
     party_walls_nl_df = pd.read_csv(output_data_dir / "party_walls_nl.csv")
     result = cityjsonfeatures_with_party_walls_nl(
-        context=build_asset_context(
-            resources={"file_store": resource_file_store}
-        ),
+        context=context,
         party_walls_nl=party_walls_nl_df,
         features_file_index=features_file_index_dict
     )
-    print(result)
+    assert result[0].stem == 'NL.IMBAG.Pand.0307100000308298.city'
+    assert result[0].suffix == '.jsonl'
