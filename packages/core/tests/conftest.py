b33621fa8d440bb2d850c9c4de92dbfb06d79ed0
gstavropoulou <gstavropoulou@gmail.comcd>
2024-08-01 16:07:21 +0200

    more ahn tests

--- packages/core/tests/conftest.py
+++ packages/core/tests/conftest.py
@@ -19,6 +19,29 @@ PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DB")
 
 
+@pytest.fixture(scope="session")
+def test_data_dir():
+    yield Path(LOCAL_DIR)
+
+
+@pytest.fixture(scope="session")
+def root_data_dir(test_data_dir) -> Path:
+    """Root directory path for test data"""
+    return test_data_dir / "reconstruction_data"
+
+
+@pytest.fixture(scope="session")
+def input_data_dir(root_data_dir) -> Path:
+    """Directory for input data"""
+    return root_data_dir / "input"
+
+
+@pytest.fixture(scope="session")
+def export_dir_uncompressed(input_data_dir) -> Path:
+    """3D BAG exported data before compression"""
+    return input_data_dir / "export_uncompressed"
+
+
 @pytest.fixture(scope="function")
 def docker_gdal_image():
     """The GDAL docker image to use for the tests"""
@@ -42,6 +65,7 @@ def database():
 @pytest.fixture
 def context(database, docker_gdal_image, wkt_testarea, tmp_path):
     yield build_op_context(
+        partition_key='01cz1',
         op_config={
             "geofilter": wkt_testarea,
             "featuretypes": ["gebouw", ]
@@ -87,3 +111,22 @@ def setenv():
 def test_data_dir():
     yield Path(LOCAL_DIR)
 
+
+@pytest.fixture(scope="session")
+def md5_pdok_ahn3_fix():
+    yield {"C_01CZ1.LAZ": "063b23d038f97576d279fb7d8a1481ad"}
+
+
+@pytest.fixture(scope="session")
+def md5_pdok_ahn4_fix():
+    yield {"C_01CZ1.LAZ": "56c731a1814dd73c79a0a5347f8a04c7"}
+
+
+@pytest.fixture(scope="session")
+def tile_index_ahn3_pdok_fix():
+    yield {'01cz1':{'type': 'Feature', 'id': 1706, 'geometry': {'type': 'Polygon', 'coordinates': [[[141000, 600000], [141000, 602500], [145000, 602500], [145000, 600000], [141000, 600000]]]}, 'properties': {'OBJECTID': 1706, 'CenterX': 143000, 'CenterY': 601250, 'Shape_Leng': 13000, 'ahn2_05m_i': 'http://geodata.nationaalgeoregister.nl/ahn2/extract/ahn2_05m_int/i01cz1.tif.zip', 'ahn2_05m_n': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/DTM_50cm/i01cz1.tif.zip', 'ahn2_05m_r': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/DSM_50cm/r01cz1.tif.zip', 'ahn2_5m': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/DTM_5m/ahn2_5_01cz1.tif.zip', 'ahn2_LAZ_g': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/ahn2_laz_units_gefilterd/g01cz1.laz', 'ahn2_LAZ_u': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/ahn2_laz_units_uitgefilterd/u01cz1.laz', 'Kaartblad': '01cz1', 'ahn1_5m': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN1/DTM_5m/01cz1.tif.zip', 'ahn1_LAZ_g': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN1/ahn1_gefilterd/01cz1.laz.zip', 'ahn1_LAZ_u': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN1/ahn1_uitgefilterd/u01cz1.laz.zip', 'AHN3_05m_DSM': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DSM_50cm/R_01CZ1.zip', 'AHN3_05m_DTM': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DTM_50cm/M_01CZ1.zip', 'AHN3_5m_DSM': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DSM_5m/R5_01CZ1.zip', 'AHN3_5m_DTM': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DTM_5m/M5_01CZ1.zip', 'AHN3_LAZ': 'https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/LAZ/C_01CZ1.LAZ', 'AHN_Beschikbaar': 'AHN1, AHN2, AHN3'}}}
+
+
+@pytest.fixture(scope="session")
+def tile_index_ahn4_pdok_fix():
+    yield {'01cz1':{'type': 'Feature', 'id': 1, 'geometry': {'type': 'Polygon', 'coordinates': [[[140000, 600000], [140000, 606250], [145000, 606250], [145000, 600000], [140000, 600000]]]}, 'properties': {'OBJECTID': 1, 'Name': '01CZ1', 'AHN4_DTM_05m': 'https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/02a_DTM_0.5m/M_01CZ1.zip', 'AHN4_DTM_5m': 'https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/02b_DTM_5m/M5_01CZ1.zip', 'AHN4_DSM_05m': 'https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/03a_DSM_0.5m/R_01CZ1.zip', 'AHN4_DSM_5m': 'https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/03b_DSM_5m/R5_01CZ1.zip', 'AHN4_LAZ': 'https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/01_LAZ/C_01CZ1.LAZ', 'Shape__Area': 31250000, 'Shape__Length': 22500}}}
--- packages/core/tests/test_assets_ahn.py
+++ packages/core/tests/test_assets_ahn.py
@@ -1,7 +1,6 @@
 import pytest
-from bag3d.core.assets.ahn.core import download_ahn_index_esri, generate_grid
-from bag3d.core.assets.ahn.download import URL_LAZ_SHA, get_md5_pdok
-
+from bag3d.core.assets.ahn.core import download_ahn_index_esri, generate_grid, ahn_laz_dir
+from bag3d.core.assets.ahn.download import URL_LAZ_SHA, get_md5_pdok, md5_pdok_ahn3, md5_pdok_ahn4, tile_index_ahn3_pdok, tile_index_ahn4_pdok, laz_files_ahn3, laz_files_ahn4
 
 @pytest.mark.parametrize("ahn_version", (3, 4), ids=("ahn3", "ahn4"))
 def test_download_ahn_index_esri(ahn_version):
@@ -26,9 +25,46 @@ def test_get_md5_pdok(url):
     assert len(md5_pdok) > 0
     for k, sha in list(md5_pdok.items())[:5]:
         assert sha is not None
-        print(k, sha)
+
 
 def test_generate_grid():
     PDOK_TILE_INDEX_BBOX = (13000, 306250, 279000, 616250)
     grid = generate_grid(PDOK_TILE_INDEX_BBOX, 200)
     assert grid == ((13000, 306250, 279000, 616250), 1330, 1550)
+
+
+def test_md5_pdok_ahn(context):
+    res = md5_pdok_ahn3(context)
+    assert len(res) > 0
+    for k, sha in list(res.items())[:5]:
+        assert sha is not None
+    res = md5_pdok_ahn4(context)
+    assert len(res) > 0
+    for k, sha in list(res.items())[:5]:
+        assert sha is not None
+
+
+def test_tile_index_ahn_pdok(context):
+    res = tile_index_ahn3_pdok(context)
+    assert len(res) > 0
+    assert res[list(res.keys())[0]] is not None
+
+    res = tile_index_ahn4_pdok(context)
+    assert len(res) > 0
+    assert res[list(res.keys())[0]] is not None
+
+
+def test_laz_files_ahn3(context, md5_pdok_ahn3_fix, tile_index_ahn3_pdok_fix):
+    laz_dir = ahn_laz_dir(context.resources.file_store.data_dir, 3)
+    laz_dir.mkdir(exist_ok=True, parents=True)
+    res = laz_files_ahn3(context, md5_pdok_ahn3_fix, tile_index_ahn3_pdok_fix)
+    assert res is not None
+
+
+def test_laz_files_ahn4(context, md5_pdok_ahn4_fix, tile_index_ahn4_pdok_fix):
+    laz_dir = ahn_laz_dir(context.resources.file_store.data_dir, 4)
+    laz_dir.mkdir(exist_ok=True, parents=True)
+    res = laz_files_ahn4(context, md5_pdok_ahn4_fix, tile_index_ahn4_pdok_fix)
+    assert res is not None
+
+
