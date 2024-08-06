4799625739fe7c50356de7d9ae98107dc743a783
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-08-06 13:37:09 +0200

    removed unnecessary resources

--- packages/floors_estimation/tests/test_integration.py
+++ packages/floors_estimation/tests/test_integration.py
@@ -3,17 +3,13 @@ from bag3d.common.resources.files import file_store
 from dagster import ExecuteInProcessResult
 
 
-def test_job_floors_estimation(database, test_data_dir, input_data_dir, model_dir):
+def test_job_floors_estimation(input_data_dir):
     resolved_job = defs.get_job_def("floors_estimation")
 
     resources={
-            "db_connection": database,
-            "file_store": file_store.configured(
-                {"data_dir": str(test_data_dir), }),
             "file_store_fastssd": file_store.configured(
                 {"data_dir": str(input_data_dir),}
-            ),
-            "model_store": model_dir,
+            )
         }
     result = resolved_job.execute_in_process(resources=resources)
 
