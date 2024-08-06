0fb4490de0d92dfd531f7cc8c6f76c89bfcc20cb
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-08-06 17:01:06 +0200

    removed upsream WIP

--- packages/party_walls/tests/test_integration.py
+++ packages/party_walls/tests/test_integration.py
@@ -1,16 +1,41 @@
-from bag3d.party_walls.code_location import defs
+from bag3d.party_walls.jobs import job_nl_party_walls
 from bag3d.common.resources.files import file_store
-from dagster import ExecuteInProcessResult
+from dagster import ExecuteInProcessResult, asset, Definitions
+import pickle
+from pathlib import Path
+from bag3d.party_walls.assets.party_walls import TilesFilesIndex, party_walls_nl, cityjsonfeatures_with_party_walls_nl
 
 
-def test_job_party_walls(input_data_dir, export_dir_uncompressed):
+
+@asset(name="features_file_index")
+def mock_features_file_index(output_data_dir):
+    return pickle.load(open(output_data_dir / "features_file_index.pkl", "rb"))
+
+
+@asset(name="distribution_tiles_files_index")
+def mock_distribution_tiles_files_index(output_data_dir):
+    return pickle.load(open(output_data_dir / "distribution_tiles_files_index.pkl", "rb"))
+
+
+def test_job_party_walls(database, input_data_dir, export_dir_uncompressed, output_data_dir):
     resources={
+            "db_connection": database,
             "file_store": file_store.configured(
                 {"data_dir": str(export_dir_uncompressed), }),
             "file_store_fastssd": file_store.configured(
                 {"data_dir": str(input_data_dir),}
             )
         }
+    defs = Definitions(
+    resources=resources,
+    assets=[mock_distribution_tiles_files_index(output_data_dir),
+            mock_features_file_index(output_data_dir),
+            party_walls_nl,
+            cityjsonfeatures_with_party_walls_nl
+            ],
+    jobs=[job_nl_party_walls, ]
+    )
+
 
     resolved_job = defs.get_job_def("nl_party_walls")
 
