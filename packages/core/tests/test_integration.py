f5847bcc81abc95de295ea3115d6ebf7fa3ac342
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-08-07 10:05:11 +0200

    core integration WIP

--- packages/core/tests/test_integration.py
+++ packages/core/tests/test_integration.py
@@ -1,10 +1,27 @@
 from bag3d.core.code_location import defs
+from bag3d.common.resources import gdal
+from bag3d.common.resources.files import file_store
+from dagster import ExecuteInProcessResult
 
 
-def test_job_nl_reconstruct():
+def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
     resolved_job = defs.get_job_def("nl_reconstruct")
     print(resolved_job)
-    result = resolved_job.execute_in_process()
+    resources = {
+        "gdal": gdal.configured({"docker": {"image": docker_gdal_image}}),
+        "db_connection": database,
+        "file_store": file_store.configured(
+            {
+                "data_dir": str(test_data_dir),
+            }
+        ),
+        "file_store_fastssd": file_store.configured(
+            {
+                "data_dir": str(test_data_dir / "reconstruction_data" / "input"),
+            }
+        ),
+    }
+    result = resolved_job.execute_in_process(resources=resources)
 
     assert isinstance(result, ExecuteInProcessResult)
     assert result.success
