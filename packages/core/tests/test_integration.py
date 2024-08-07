6d225b1430059169e93be5d1db9684098315f806
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-08-07 10:49:18 +0200

    reconstruction integration WIP

--- packages/core/tests/test_integration.py
+++ packages/core/tests/test_integration.py
@@ -1,13 +1,82 @@
-from bag3d.core.code_location import defs
 from bag3d.common.resources import gdal
 from bag3d.common.resources.files import file_store
 from dagster import ExecuteInProcessResult
+import pickle
 
+from bag3d.common.resources.files import file_store
+from bag3d.core.assets import (reconstruction)
+from bag3d.core.jobs import job_nl_reconstruct
+from bag3d.common.resources.executables import geoflow, roofer
+
+from dagster import (AssetKey, Definitions, ExecuteInProcessResult, IOManager,
+                     SourceAsset, load_assets_from_package_module, Output)
+from bag3d.common.types import PostgresTableIdentifier
+from bag3d.core.assets.input import RECONSTRUCTION_INPUT_SCHEMA
+
+
+def mock_reconstruction_input(reconstruction_input):
+    class MockIOManager(IOManager):
+        def load_input(self, context):
+            new_table = PostgresTableIdentifier(RECONSTRUCTION_INPUT_SCHEMA,
+                                                "reconstruction_input")
+            return new_table
+
+        def handle_output(self, context, obj):
+            raise NotImplementedError()
+
+    return SourceAsset(
+        key=AssetKey(["input", reconstruction_input]),
+        io_manager_def=MockIOManager(),
+    )
+
+def mock_tiles(tiles):
+    class MockIOManager(IOManager):
+        def load_input(self, context):
+            new_table = PostgresTableIdentifier(RECONSTRUCTION_INPUT_SCHEMA,
+                                                "tiles")
+            return new_table
+
+        def handle_output(self, context, obj):
+            raise NotImplementedError()
+
+    return SourceAsset(
+        key=AssetKey(["input", tiles]),
+        io_manager_def=MockIOManager(),
+    )
+
+def mock_index(index):
+    class MockIOManager(IOManager):
+        def load_input(self, context):
+            new_table = PostgresTableIdentifier(RECONSTRUCTION_INPUT_SCHEMA,
+                                                "index")
+            return new_table
+
+        def handle_output(self, context, obj):
+            raise NotImplementedError()
+
+    return SourceAsset(
+        key=AssetKey(["input", index]),
+        io_manager_def=MockIOManager(),
+    )
+
+def mock_regular_grid_200m(regular_grid_200m):
+    class MockIOManager(IOManager):
+        def load_input(self, context):
+            new_table = PostgresTableIdentifier("ahn", "regular_grid_200m")
+            return new_table
+
+        def handle_output(self, context, obj):
+            raise NotImplementedError()
+
+    return SourceAsset(
+        key=AssetKey(["ahn", regular_grid_200m]),
+        io_manager_def=MockIOManager(),
+    )
 
 def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
-    resolved_job = defs.get_job_def("nl_reconstruct")
-    print(resolved_job)
     resources = {
+        "geoflow": geoflow,
+        "roofer": roofer,
         "gdal": gdal.configured({"docker": {"image": docker_gdal_image}}),
         "db_connection": database,
         "file_store": file_store.configured(
@@ -21,7 +90,41 @@ def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
             }
         ),
     }
-    result = resolved_job.execute_in_process(resources=resources)
+
+    all_reconstruction_assets = load_assets_from_package_module(
+        reconstruction, key_prefix="reconstruction", group_name="reconstruction"
+    )
+    
+    # Filter the assets to include only the ones we need
+    reconstruction_assets = [
+        asset
+        for asset in all_reconstruction_assets
+        if asset.key
+        in {
+            AssetKey(["reconstruction", "cropped_input_and_config_nl"]),
+            AssetKey(["reconstruction", "reconstructed_building_models_nl"]),
+        }
+    ]
+
+    defs = Definitions(
+        resources=resources,
+        assets=[
+            mock_regular_grid_200m(
+                "regular_grid_200m"
+            ),
+            mock_reconstruction_input("reconstruction_input"),
+            mock_tiles("tiles"),
+            mock_index("index"),
+            reconstruction_assets[0],
+            reconstruction_assets[1],
+        ],
+        jobs=[
+            job_nl_reconstruct,
+        ],
+    )
+
+    resolved_job = defs.get_job_def("nl_reconstruct")
+    result = resolved_job.execute_in_process(resources=resources, partition_key="10/564/624")
 
     assert isinstance(result, ExecuteInProcessResult)
     assert result.success
