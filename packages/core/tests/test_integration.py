e5450db3e3e001100e52bff35524f71ceaa0bdec
gstavropoulou <gstavropoulou@gmail.comcd>
2024-08-14 13:31:18 +0200

    core integration nl_export

--- packages/core/tests/test_integration.py
+++ packages/core/tests/test_integration.py
@@ -2,16 +2,17 @@ import pickle
 
 import pytest
 from bag3d.common.resources import gdal
-from bag3d.common.resources.executables import geoflow, roofer
+from bag3d.common.resources.executables import geoflow, roofer, tyler
 from bag3d.common.resources.files import file_store
 from bag3d.common.resources.temp_until_configurableresource import (
-    EXE_PATH_GEOF, EXE_PATH_ROOFER_CROP, FLOWCHART_PATH_RECONSTRUCT)
+    EXE_PATH_GEOF, EXE_PATH_ROOFER_CROP, FLOWCHART_PATH_RECONSTRUCT, EXE_PATH_TYLER_DB, EXE_PATH_TYLER)
 from bag3d.common.types import PostgresTableIdentifier
-from bag3d.core.assets import reconstruction
+from bag3d.core.assets import reconstruction, export, deploy
 from bag3d.core.assets.input import RECONSTRUCTION_INPUT_SCHEMA
-from bag3d.core.jobs import job_nl_reconstruct
+from bag3d.core.jobs import job_nl_reconstruct, job_nl_export, job_nl_deploy
 from dagster import (AssetKey, Definitions, ExecuteInProcessResult, IOManager,
                      Output, SourceAsset, load_assets_from_package_module)
+from bag3d.core.assets.export.tile import reconstruction_output_tiles_func
 
 
 def mock_reconstruction_input(reconstruction_input):
@@ -76,9 +77,15 @@ def mock_regular_grid_200m(regular_grid_200m):
     )
 
 
-@pytest.mark.slow
+#@pytest.mark.slow
 def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
     resources = {
+        "tyler": tyler.configured({
+    "exes": {
+        "tyler-db": EXE_PATH_TYLER_DB,
+        "tyler": EXE_PATH_TYLER
+    }
+}),
         "geoflow": geoflow.configured(
             {
                 "exes": {"geof": EXE_PATH_GEOF},
@@ -90,11 +97,17 @@ def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
                 "exes": {"crop": EXE_PATH_ROOFER_CROP},
             }
         ),
-        "gdal": gdal.configured({"docker": {"image": docker_gdal_image}}),
+        "gdal": gdal.configured({
+            "exes": {
+                "ogr2ogr": "/opt/bin/ogr2ogr",
+                "ogrinfo": "/opt/bin/ogrinfo",
+                "sozip": "/opt/bin/sozip"
+            }
+        }),
         "db_connection": database,
         "file_store": file_store.configured(
             {
-                "data_dir": str(test_data_dir),
+                "data_dir": str(test_data_dir / "reconstruction_data" / "input"),
             }
         ),
         "file_store_fastssd": file_store.configured(
@@ -119,6 +132,14 @@ def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
         }
     ]
 
+    all_export_assets = load_assets_from_package_module(
+        export, key_prefix="export", group_name="export"
+    )
+
+    # all_deploy_assets = load_assets_from_package_module(
+    #     deploy, key_prefix="deploy", group_name="deploy"
+    # )
+
     defs = Definitions(
         resources=resources,
         assets=[
@@ -126,11 +147,14 @@ def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
             mock_reconstruction_input("reconstruction_input"),
             mock_tiles("tiles"),
             mock_index("index"),
-            reconstruction_assets[0],
-            reconstruction_assets[1],
+            *reconstruction_assets,
+            *all_export_assets,
+            #*all_deploy_assets
         ],
         jobs=[
             job_nl_reconstruct,
+            job_nl_export,
+           # job_nl_deploy,
         ],
     )
 
@@ -141,3 +165,17 @@ def test_job_nl_reconstruct(database, docker_gdal_image, test_data_dir):
 
     assert isinstance(result, ExecuteInProcessResult)
     assert result.success
+
+    resolved_job = defs.get_job_def("nl_export")
+    result = resolved_job.execute_in_process(
+        resources=resources
+    )
+
+    assert isinstance(result, ExecuteInProcessResult)
+    assert result.success
+
+
+    # resolved_job = defs.get_job_def("nl_deploy")
+    # result = resolved_job.execute_in_process(
+    #     resources=resources, partition_key="10/564/624"
+    # )
