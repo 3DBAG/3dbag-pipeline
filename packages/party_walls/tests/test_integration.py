f8f69cf9a12d0eeb17a1f73193dbb8df9aae968b
gstavropoulou <gstavropoulou@gmail.comcd>
2024-08-14 13:30:59 +0200

    intrmediate data party walls

--- packages/party_walls/tests/test_integration.py
+++ packages/party_walls/tests/test_integration.py
@@ -1,4 +1,5 @@
 import pickle
+from pathlib import Path
 
 import pytest
 from bag3d.common.resources.files import file_store
@@ -8,10 +9,13 @@ from dagster import (AssetKey, Definitions, ExecuteInProcessResult, IOManager,
                      SourceAsset, load_assets_from_package_module)
 
 
-def mock_features_file_index(features_file_index, output_data_dir):
+def mock_features_file_index(features_file_index, intermediate_data_dir, input_data_dir):
     class MockIOManager(IOManager):
         def load_input(self, context):
-            return pickle.load(open(output_data_dir / "features_file_index.pkl", "rb"))
+            data =  pickle.load(open(intermediate_data_dir / "features_file_index.pkl", "rb"))
+            for k,v in data.items():
+                data[k] = Path(str(v).replace(str(v.parents[8]), str(input_data_dir)))
+            return data
 
         def handle_output(self, context, obj):  # pragma: no cover
             raise NotImplementedError()
@@ -23,13 +27,15 @@ def mock_features_file_index(features_file_index, output_data_dir):
 
 
 def mock_distribution_tiles_files_index(
-    distribution_tiles_files_index, output_data_dir
-):
+    distribution_tiles_files_index, intermediate_data_dir, input_data_dir):
     class MockIOManager(IOManager):
         def load_input(self, context):
-            return pickle.load(
-                open(output_data_dir / "distribution_tiles_files_index.pkl", "rb")
+            data =  pickle.load(
+                open(intermediate_data_dir / "distribution_tiles_files_index.pkl", "rb")
             )
+            for i, d in enumerate(data.paths_array):
+                data.paths_array[i] = Path(str(d).replace(str(d.parents[7]), str(input_data_dir)))
+            return data
 
         def handle_output(self, context, obj):  # pragma: no cover
             raise NotImplementedError()
@@ -40,9 +46,9 @@ def mock_distribution_tiles_files_index(
     )
 
 
-@pytest.mark.slow
+#@pytest.mark.slow
 def test_job_party_walls(
-    database, input_data_dir, export_dir_uncompressed, output_data_dir
+    database, input_data_dir, export_dir_uncompressed, intermediate_data_dir
 ):
     resources = {
         "db_connection": database,
@@ -75,9 +81,8 @@ def test_job_party_walls(
         resources=resources,
         assets=[
             mock_distribution_tiles_files_index(
-                "distribution_tiles_files_index", output_data_dir
-            ),
-            mock_features_file_index("features_file_index", output_data_dir),
+                "distribution_tiles_files_index", intermediate_data_dir, input_data_dir),
+            mock_features_file_index("features_file_index", intermediate_data_dir, input_data_dir),
             party_assets[0],
             party_assets[1],
         ],
