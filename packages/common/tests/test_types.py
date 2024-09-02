a5cc46fa10fe138dad8ff5a3f67cb2535b4922d2
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-09-02 14:53:04 +0200

    expanded core test types

--- packages/common/tests/test_types.py
+++ packages/common/tests/test_types.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from bag3d.common.types import ExportResult
 
 
-def test_export_result_to_dict():
-    """Can we cast an ExportResult to a dict?"""
+def test_export_result():
+    """Test ExportResult class"""
     export_result = ExportResult(
         tile_id="z/x/y",
         cityjson_path=Path("/data"),
@@ -16,3 +16,7 @@ def test_export_result_to_dict():
     )
     d = dict(export_result)
     assert d["tile_id"] == "z/x/y"
+    assert export_result.has_cityjson
+    assert export_result.has_gpkg
+    assert export_result.has_obj
+
