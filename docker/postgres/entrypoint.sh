0d46ef5d8f79815122f1c16e6a4ef1434fba899c
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-09-03 15:53:32 +0200

    removed empty db

--- docker/postgres/entrypoint.sh
+++ docker/postgres/entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DB} --no-owner /var/lib/postgresql/data/baseregisters.tar
-psql -d postgres -U ${POSTGRES_USER} -c "create database baseregisters_empty;"
-psql -d baseregisters_empty -c "create extension postgis;"
-
 
