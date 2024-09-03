aeee9d72ce68f37e66b823cf8d6a7b86daec1e69
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-09-03 13:21:26 +0200

    tmp_path for test_export_result

--- packages/common/tests/test_types.py
+++ packages/common/tests/test_types.py
@@ -3,14 +3,14 @@ from pathlib import Path
 from bag3d.common.types import ExportResult
 
 
-def test_export_result():
+def test_export_result(tmp_path):
     """Test ExportResult class"""
     export_result = ExportResult(
         tile_id="z/x/y",
-        cityjson_path=Path("/data"),
-        gpkg_path=Path("/data"),
+        cityjson_path=tmp_path,
+        gpkg_path=tmp_path,
         obj_paths=[
-            Path("/data/file.obj"),
+            Path(tmp_path),
         ],
         wkt="POLYGON((0 0, 1 1))",
     )
