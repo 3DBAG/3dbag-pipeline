08e603cbd808b11ab81705966be0954dc668d605
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-09-09 10:45:03 +0200

    gdal in core

--- packages/core/tests/conftest.py
+++ packages/core/tests/conftest.py
@@ -2,9 +2,12 @@ import os
 from pathlib import Path
 
 import pytest
-from bag3d.common.resources import gdal
 from bag3d.common.resources.database import DatabaseConnection
-from bag3d.common.resources.executables import DOCKER_GDAL_IMAGE
+from bag3d.common.resources.executables import (
+    DOCKER_GDAL_IMAGE,
+    GdalResource,
+    DockerConfig,
+)
 from bag3d.common.resources.files import file_store
 from bag3d.common.types import PostgresTableIdentifier
 from bag3d.core.assets.input import RECONSTRUCTION_INPUT_SCHEMA
@@ -18,10 +21,11 @@ PASSWORD = os.getenv("BAG3D_PG_PASSWORD")
 DB_NAME = os.getenv("BAG3D_PG_DATABASE")
 
 
-@pytest.fixture(scope="function")
-def docker_gdal_image():
-    """The GDAL docker image to use for the tests"""
-    return DOCKER_GDAL_IMAGE
+@pytest.fixture(scope="session")
+def gdal():
+    yield GdalResource(
+        docker_cfg=DockerConfig(image=DOCKER_GDAL_IMAGE, mount_point="/tmp")
+    ).gdal
 
 
 @pytest.fixture(scope="function")
@@ -39,7 +43,7 @@ def database():
 
 
 @pytest.fixture
-def context(database, docker_gdal_image, wkt_testarea, tmp_path):
+def context(database, wkt_testarea, tmp_path, gdal):
     yield build_op_context(
         partition_key="01cz1",
         op_config={
@@ -49,7 +53,7 @@ def context(database, docker_gdal_image, wkt_testarea, tmp_path):
             ],
         },
         resources={
-            "gdal": gdal.configured({"docker": {"image": docker_gdal_image}}),
+            "gdal": gdal,
             "db_connection": database,
             "file_store": file_store.configured(
                 {
