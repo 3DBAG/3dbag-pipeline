c2540d7eca9d2cbb21eb79df9719ab0f7856c0e4
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-09-18 21:32:38 +0200

    WIP correcting tests

--- packages/party_walls/tests/test_party_walls.py
+++ packages/party_walls/tests/test_party_walls.py
@@ -28,9 +28,9 @@ def test_party_walls(context, mock_distribution_tiles_files_index):
 
 
 def test_features_file_index(context):
-    """Can we find and map all the 5829 cityjson feature files of the test data?"""
+    """Can we find and map all the 5825 cityjson feature files of the test data?"""
     result = features_file_index(context=context)
-    assert len(result) == 5829
+    assert len(result) == 5825
 
 
 @pytest.mark.slow
