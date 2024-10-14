37b89b74d015ba08619c73ff21c02c2a343497c4
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-10-14 15:00:35 +0200

    fixed tests

--- packages/core/tests/conftest.py
+++ packages/core/tests/conftest.py
@@ -114,77 +114,24 @@ def md5_pdok_ahn4_fix():
 
 
 @pytest.fixture(scope="session")
-def tile_index_ahn3_pdok_fix():
+def tile_index_pdok_fix():
     yield {
         "01cz1": {
-            "type": "Feature",
-            "id": 1706,
+            "AHN3_LAZ": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/LAZ/C_01CZ1.LAZ",
+            "AHN4_LAZ": "https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/01_LAZ/C_01CZ1.LAZ",
+            "AHN5_LAZ": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN5/01_LAZ/2023_C_01CZ1.LAZ",
             "geometry": {
                 "type": "Polygon",
                 "coordinates": [
                     [
-                        [141000, 600000],
-                        [141000, 602500],
-                        [145000, 602500],
-                        [145000, 600000],
-                        [141000, 600000],
+                        [140000.0001043251, 600000.0005214083],
+                        [140000.00010435135, 606250.0005244487],
+                        [145000.0001086861, 606250.0005244645],
+                        [145000.0001086705, 600000.0005214227],
+                        [140000.0001043251, 600000.0005214083],
                     ]
                 ],
-            },
-            "properties": {
-                "OBJECTID": 1706,
-                "CenterX": 143000,
-                "CenterY": 601250,
-                "Shape_Leng": 13000,
-                "ahn2_05m_i": "http://geodata.nationaalgeoregister.nl/ahn2/extract/ahn2_05m_int/i01cz1.tif.zip",
-                "ahn2_05m_n": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/DTM_50cm/i01cz1.tif.zip",
-                "ahn2_05m_r": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/DSM_50cm/r01cz1.tif.zip",
-                "ahn2_5m": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/DTM_5m/ahn2_5_01cz1.tif.zip",
-                "ahn2_LAZ_g": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/ahn2_laz_units_gefilterd/g01cz1.laz",
-                "ahn2_LAZ_u": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN2/ahn2_laz_units_uitgefilterd/u01cz1.laz",
-                "Kaartblad": "01cz1",
-                "ahn1_5m": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN1/DTM_5m/01cz1.tif.zip",
-                "ahn1_LAZ_g": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN1/ahn1_gefilterd/01cz1.laz.zip",
-                "ahn1_LAZ_u": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN1/ahn1_uitgefilterd/u01cz1.laz.zip",
-                "AHN3_05m_DSM": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DSM_50cm/R_01CZ1.zip",
-                "AHN3_05m_DTM": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DTM_50cm/M_01CZ1.zip",
-                "AHN3_5m_DSM": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DSM_5m/R5_01CZ1.zip",
-                "AHN3_5m_DTM": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/DTM_5m/M5_01CZ1.zip",
-                "AHN3_LAZ": "https://ns_hwh.fundaments.nl/hwh-ahn/AHN3/LAZ/C_01CZ1.LAZ",
-                "AHN_Beschikbaar": "AHN1, AHN2, AHN3",
-            },
-        }
-    }
-
-
-@pytest.fixture(scope="session")
-def tile_index_ahn4_pdok_fix():
-    yield {
-        "01cz1": {
-            "type": "Feature",
-            "id": 1,
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [
-                    [
-                        [140000, 600000],
-                        [140000, 606250],
-                        [145000, 606250],
-                        [145000, 600000],
-                        [140000, 600000],
-                    ]
-                ],
-            },
-            "properties": {
-                "OBJECTID": 1,
-                "Name": "01CZ1",
-                "AHN4_DTM_05m": "https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/02a_DTM_0.5m/M_01CZ1.zip",
-                "AHN4_DTM_5m": "https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/02b_DTM_5m/M5_01CZ1.zip",
-                "AHN4_DSM_05m": "https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/03a_DSM_0.5m/R_01CZ1.zip",
-                "AHN4_DSM_5m": "https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/03b_DSM_5m/R5_01CZ1.zip",
-                "AHN4_LAZ": "https://ns_hwh.fundaments.nl/hwh-ahn/ahn4/01_LAZ/C_01CZ1.LAZ",
-                "Shape__Area": 31250000,
-                "Shape__Length": 22500,
+                "geometry_name": "shape",
             },
         }
     }
--- packages/core/tests/test_assets_ahn.py
+++ packages/core/tests/test_assets_ahn.py
@@ -64,19 +64,19 @@ def test_tile_index_pdok(context):
 
 
 @pytest.mark.slow
-def test_laz_files_ahn3(context, md5_pdok_ahn3_fix, tile_index_ahn3_pdok_fix):
+def test_laz_files_ahn3(context, md5_pdok_ahn3_fix, tile_index_pdok_fix):
     laz_dir = ahn_laz_dir(context.resources.file_store.file_store.data_dir, 3)
     laz_dir.mkdir(exist_ok=True, parents=True)
-    res = laz_files_ahn3(context, md5_pdok_ahn3_fix, tile_index_ahn3_pdok_fix)
+    res = laz_files_ahn3(context, md5_pdok_ahn3_fix, tile_index_pdok_fix)
     assert res is not None
     print(res.value)
 
 
 @pytest.mark.slow
-def test_laz_files_ahn4(context, md5_pdok_ahn4_fix, tile_index_ahn4_pdok_fix):
+def test_laz_files_ahn4(context, md5_pdok_ahn4_fix, tile_index_pdok_fix):
     laz_dir = ahn_laz_dir(context.resources.file_store.file_store.data_dir, 4)
     laz_dir.mkdir(exist_ok=True, parents=True)
-    res = laz_files_ahn4(context, md5_pdok_ahn4_fix, tile_index_ahn4_pdok_fix)
+    res = laz_files_ahn4(context, md5_pdok_ahn4_fix, tile_index_pdok_fix)
     assert res is not None
 
 
