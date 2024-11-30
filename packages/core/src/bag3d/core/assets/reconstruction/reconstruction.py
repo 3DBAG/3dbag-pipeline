7525437aaaccba94f053cca202a0ee8d5687716c
Balázs Dukai <balazs.dukai@gmail.com>
2024-11-30 04:40:41 +0100

    Include AHN5 in reconstruction

--- packages/core/src/bag3d/core/assets/reconstruction/reconstruction.py
+++ packages/core/src/bag3d/core/assets/reconstruction/reconstruction.py
@@ -165,6 +165,11 @@ def create_roofer_config(
     name = "AHN4"
     quality = 0
     source = {ahn4_files}
+    
+    [[pointclouds]]
+    name = "AHN5"
+    quality = 2
+    source = {ahn5_files}
 
     [output-attributes]
     status = "b3_status"
@@ -231,9 +236,12 @@ def create_roofer_config(
     laz_files_ahn4 = [
         str(out_dir_ahn4 / f"t_{tile_id_ahn[0]}.laz") for tile_id_ahn in res
     ]
-    out_dir_ahn5 = ahn_dir(
-        context.resources.file_store.file_store.data_dir, ahn_version=5
-    ).joinpath("tiles_200m")
+    if dir_tiles_200m_ahn5 is not None:
+        out_dir_ahn5 = Path(dir_tiles_200m_ahn5)
+    else:
+        out_dir_ahn5 = ahn_dir(
+            context.resources.file_store.file_store.data_dir, ahn_version=5
+        ).joinpath("tiles_200m")
     laz_files_ahn5 = [
         str(out_dir_ahn5 / f"t_{tile_id_ahn[0]}.laz") for tile_id_ahn in res
     ]
