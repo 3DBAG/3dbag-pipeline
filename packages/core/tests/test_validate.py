175139bde99aec913c310366bde584cb497bf5ba
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-02-25 21:04:02 +0100

    formatting

--- packages/core/tests/test_validate.py
+++ packages/core/tests/test_validate.py
@@ -4,8 +4,7 @@ from bag3d.core.assets.export import validate
 def test_obj(context, test_data_dir):
     res = validate.obj(
         context.resources.validation.app,
-        test_data_dir
-        / "validation_input/",
+        test_data_dir / "validation_input/",
         "10-564-624",
         planarity_n_tol=20.0,
         planarity_d2p_tol=0.001,
@@ -19,8 +18,7 @@ def test_obj(context, test_data_dir):
 def test_gpkg(context, test_data_dir):
     res = validate.gpkg(
         context.resources.gdal.app,
-        test_data_dir
-        / "validation_input/",
+        test_data_dir / "validation_input/",
         "10-564-624",
         "https://data.3dbag.nl",
         "test",
@@ -32,8 +30,7 @@ def test_gpkg(context, test_data_dir):
 def test_cityjson(context, test_data_dir):
     res = validate.cityjson(
         context.resources.validation.app,
-        test_data_dir
-        / "validation_input/",
+        test_data_dir / "validation_input/",
         "10-564-624",
         planarity_n_tol=20.0,
         planarity_d2p_tol=0.001,
