9d5d74fc16fc45cd738d7ef419af88d24c81261b
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-03-14 17:17:34 +0100

    forgot to remove rust

--- tools-build.sh
+++ tools-build.sh
@@ -162,12 +162,6 @@ handle_options() {
 handle_options "$@"
 cd $root_dir || exit
 
-# Install Rust and Cargo
-if ! command -v cargo &> /dev/null; then
-  printf "\n\nInstalling Rust and Cargo...\n\n"
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-  source $HOME/.cargo/env
-fi
 
 if [ "$build_tyler" = true ] ; then
   printf "\n\nInstalling Tyler...\n\n"
