317b5c75dca129fbe8e11480215f1968fc722cc0
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-06-29 09:12:15 +0200

    file removal

--- packages/core/tests/test_assets_deploy.py
+++ packages/core/tests/test_assets_deploy.py
@@ -3,6 +3,7 @@ from bag3d.core.assets.deploy.godzilla import (
     downloadable_godzilla,
 )
 from pathlib import Path
+from fabric import Connection
 
 
 def test_downloadable_godzilla(context, test_data_dir):
@@ -14,28 +15,37 @@ def test_downloadable_godzilla(context, test_data_dir):
     empty_file = export_dir / "dummy.txt"
     empty_file.touch()
 
-    # compress the export dir
-    res = compressed_export_nl(context, export_dir)
-
-    compressed_file = Path(res.metadata["path"].text)
-    assert compressed_file.exists()  # Check that the file was created
-
     # Create a mock metadata file
     metadata_file = test_data_dir / "deployment" / "3DBAG" / "metadata.json"
     metadata_file.touch()
     metadata_file.write_text(
         '{"identificationInfo": {"citation": {"edition": "test_version"}}}'
     )
+    try:
+        # compress the export dir
+        res = compressed_export_nl(context, export_dir)
 
-    # Test the transfer to godzilla
-    res = downloadable_godzilla(
-        context,
-        compressed_file,
-        metadata_file,
-        data_dir="/tmp",
-        public_dir="/tmp/gina_public",
-    )
-    assert res == "/tmp/test_version"  # Check that the function returns a value
+        compressed_file = Path(res.metadata["path"].text)
+        assert compressed_file.exists()  # Check that the file was created
 
-    # Remove the file after the test
-    # path.unlink()
+        # Test the transfer to godzilla
+        res = downloadable_godzilla(
+            context,
+            compressed_file,
+            metadata_file,
+            data_dir="/tmp",
+            public_dir="/tmp/gina_public",
+        )
+        assert res == "/tmp/test_version"  # Check that the function returns a value
+    finally:
+        # Clean up the test files
+        compressed_file.unlink(missing_ok=True)
+        metadata_file.unlink(missing_ok=True)
+        empty_file.unlink(missing_ok=True)
+        export_dir.rmdir()
+        with Connection(host="godzilla.bk.tudelft.nl", user="dagster") as c:
+            c.run("rm -rf /tmp/test_version", warn=True)
+            c.run("rm -rf /tmp/gina_public", warn=True)
+            c.run("rm -f /tmp/export_test_version.tar.gz", warn=True)
+
+
