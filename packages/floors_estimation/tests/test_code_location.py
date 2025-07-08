20c2336c11cb631e0161e5b55b43d0ea482a8910
Balázs Dukai <balazs.dukai@gmail.com>
2025-07-08 06:05:08 +0200

    Test that code locations load successfully

---
+++ packages/floors_estimation/tests/test_code_location.py
@@ -0,0 +1,11 @@
+import importlib
+from dagster import Definitions
+
+
+def test_definitions_loadable():
+    # Import your definitions module
+    module = importlib.import_module("bag3d.floors_estimation.code_location")
+    defs = getattr(module, "defs")
+
+    # This will raise an error if definitions are not loadable
+    Definitions.validate_loadable(defs)
