81af48557e78ac0d9b5ad67021e66e1e23069cbc
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-07-24 15:23:09 +0200

    fixed definitions

--- packages/core/src/bag3d/core/code_location.py
+++ packages/core/src/bag3d/core/code_location.py
@@ -7,6 +7,7 @@ from bag3d.core.asset_groups import (
     reconstruction_assets,
     export_assets,
     deploy_assets,
+    release_assets,
 )
 from bag3d.core.jobs import (
     job_source_input,
@@ -28,6 +29,7 @@ all_assets = [
     *reconstruction_assets,
     *export_assets,
     *deploy_assets,
+    *release_assets,
 ]
 
 all_jobs = [
