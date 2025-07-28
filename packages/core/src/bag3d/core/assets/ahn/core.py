d23ea726d11d003e4b18f861c73b3b08dd8a9ff3
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-07-28 12:29:14 +0200

    formatting

--- packages/core/src/bag3d/core/assets/ahn/core.py
+++ packages/core/src/bag3d/core/assets/ahn/core.py
@@ -41,7 +41,7 @@ def download_ahn_index(
     with_geom: bool = False,
 ) -> Optional[Dict[str, Optional[Dict[str, Optional[str]]]]]:
     """Downloads the AHN 3/4/5 tile index.
-    Args:            
+    Args:
         with_geom: If False, request only the AHN tile ids. Else also request the
             tile boundaries as geojson.
     Returns:
