b9702ff0ec78a877c45fbd7314419d1e652d12a6
Balázs Dukai <balazs.dukai@gmail.com>
2025-08-06 11:46:01 +0200

    Fix ahn5 laz tiles input for roofer
    Fixes #249

--- packages/core/src/bag3d/core/assets/reconstruction/reconstruction.py
+++ packages/core/src/bag3d/core/assets/reconstruction/reconstruction.py
@@ -257,6 +257,16 @@ def create_roofer_config(
     laz_files_ahn4 = [
         str(out_dir_ahn4 / f"t_{tile_id_ahn[0]}.laz") for tile_id_ahn in res
     ]
+    if dir_tiles_200m_ahn5 is not None:
+        out_dir_ahn5 = Path(dir_tiles_200m_ahn5)
+    else:
+        out_dir_ahn5 = ahn_dir(
+            context.resources.file_store.file_store.data_dir, ahn_version=5
+        ).joinpath("tiles_200m")
+    # TODO: same with the laz filename pattern
+    laz_files_ahn5 = [
+        str(out_dir_ahn5 / f"t_{tile_id_ahn[0]}.laz") for tile_id_ahn in res
+    ]
     # Would be neater if we could use -sql in the OGR connection to do this query,
     # instead of creating a view.
     tile_view = PostgresTableIdentifier(tiles.schema, f"t_{tile_id}")
@@ -284,13 +294,7 @@ def create_roofer_config(
         footprint_file=f"PG:{context.resources.db_connection.connect.dsn} tables={tile_view}",
         ahn3_files=laz_files_ahn3,
         ahn4_files=laz_files_ahn4,
-        ahn5_files=[
-            str(
-                ahn_dir(
-                    context.resources.file_store.file_store.data_dir, ahn_version=5
-                ).joinpath("as_downloaded/LAZ")
-            )
-        ],
+        ahn5_files=laz_files_ahn5,
         output_path=output_dir,
     )
     path_toml = output_dir / "roofer.toml"
