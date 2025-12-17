68e636f5f707c7b1805438a67a55976bb8f18c29
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-12-17 19:36:57 +0100

    split asset files

--- packages/core/tests/test_assets_top10nl.py
+++ packages/core/tests/test_assets_top10nl.py
@@ -1,16 +1,9 @@
 import pytest
 from bag3d.core.assets.top10nl.download import extract_top10nl
-from bag3d.core.assets.bgt.download import extract_bgt
 
 
 @pytest.mark.slow
 def test_extract_top10nl(context_top10nl):
     """Does the complete asset work?"""
     res = extract_top10nl(context_top10nl)
-    assert res.value.exists()
-
-
-def test_extract_bgt(context_bgt):
-    """Does the complete asset work?"""
-    res = extract_bgt(context_bgt)
     assert res.value.exists()
