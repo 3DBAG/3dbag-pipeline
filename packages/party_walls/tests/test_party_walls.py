f3ab55d586acf0bd0638b5972c0e52d621d020f5
Balázs Dukai <balazs.dukai@gmail.com>
2026-02-24 16:40:33 +0100

    Improve type safety in `party_walls`, refactor data access, and add assertions in tests

--- packages/party_walls/tests/test_party_walls.py
+++ packages/party_walls/tests/test_party_walls.py
@@ -1,6 +1,4 @@
 import pytest
-from pathlib import Path
-from typing import cast
 from pandas import DataFrame
 from bag3d.party_walls.assets.party_walls import (
     PartyWallsConfig,
@@ -17,10 +15,8 @@ TILE_IDS = ("0/0/0",)
 def test_distribution_tiles_files_index(file_store_resource, version):
     """Can we parse the CityJSON tiles and return valid data?"""
 
-    result = cast(
-        TilesFilesIndex,
-        distribution_tiles_files_index(file_store_resource, version),
-    )
+    result = distribution_tiles_files_index(file_store_resource, version)
+    assert isinstance(result, TilesFilesIndex)
     assert len(result.tree.geometries) == len(TILE_IDS)
     assert len(result.paths_array) == len(TILE_IDS)
     result_tile_ids = tuple(sorted(result.export_results.keys()))
@@ -31,19 +27,15 @@ def test_distribution_tiles_files_index(file_store_resource, version):
 def test_party_walls(context, database, mock_distribution_tiles_files_index):
     """Can we compute the party walls and other statistics?"""
 
-    result = cast(
-        DataFrame,
-        party_walls_nl(context, mock_distribution_tiles_files_index, database),
-    )
+    result = party_walls_nl(context, mock_distribution_tiles_files_index, database)
+    assert isinstance(result, DataFrame)
     assert not result.empty
 
 
 def test_features_file_index(file_store_fastssd_resource):
     """Can we find and map all the cityjson feature files of the test data?"""
-    result = cast(
-        dict[str, Path],
-        features_file_index(PartyWallsConfig(), file_store_fastssd_resource),
-    )
+    result = features_file_index(PartyWallsConfig(), file_store_fastssd_resource)
+    assert isinstance(result, dict)
     assert len(result) == 415
 
 
@@ -52,14 +44,12 @@ def test_cityjsonfeatures_with_party_walls_nl(
     context, file_store_fastssd_resource, mock_party_walls_nl, mock_features_file_index
 ):
     """Can we create cityjsonfeatures with the party wall data?"""
-    result = cast(
-        list[Path],
-        cityjsonfeatures_with_party_walls_nl(
-            context,
-            mock_party_walls_nl,
-            mock_features_file_index,
-            file_store_fastssd_resource,
-        ),
+    result = cityjsonfeatures_with_party_walls_nl(
+        context,
+        mock_party_walls_nl,
+        mock_features_file_index,
+        file_store_fastssd_resource,
     )
+    assert isinstance(result, list)
     assert result[0].stem == "NL.IMBAG.Pand.0307100000308298.city"
     assert result[0].suffix == ".jsonl"
