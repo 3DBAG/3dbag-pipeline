cba880183d9e7b9ec6795a04d2310563c149f640
GinaStavropoulou <ginastavropoulou@gmail.com>
2026-04-01 22:12:53 +0200

    download cbs data

--- packages/core/src/bag3d/core/jobs.py
+++ packages/core/src/bag3d/core/jobs.py
@@ -67,6 +67,7 @@ job_source_input = define_asset_job(
     | AssetSelection.assets(["bag", "bag_pandactueelbestaand"])
     | AssetSelection.assets(["bag", "stage_bag_verblijfsobject"])
     | AssetSelection.assets(["bag", "bag_verblijfsobjectactueelbestaand"])
+    | AssetSelection.groups("cbs")
     | AssetSelection.groups("top10nl")
     | AssetSelection.groups("input"),
 )
