e281ca1fa65b1769e30cddc5c0459988718e1252
GinaStavropoulou <ginastavropoulou@gmail.com>
2026-07-09 15:27:54 +0200

    forgot the init

--- packages/core/src/bag3d/core/assets/ahn/__init__.py
+++ packages/core/src/bag3d/core/assets/ahn/__init__.py
@@ -1,6 +1,11 @@
+import gzip
 import json
 from pathlib import Path
 
 _dir = Path(__file__).parent
-AHN_TILE_IDS = set(json.loads((_dir / "ahn_tile_ids.json").read_text()))
-AHN6_TILE_IDS = set(json.loads((_dir / "ahn6_tile_ids.json").read_text()))
+AHN_TILE_IDS = set(
+    json.loads(gzip.decompress((_dir / "ahn_tile_ids.json.gz").read_bytes()))
+)
+AHN6_TILE_IDS = set(
+    json.loads(gzip.decompress((_dir / "ahn6_tile_ids.json.gz").read_bytes()))
+)
