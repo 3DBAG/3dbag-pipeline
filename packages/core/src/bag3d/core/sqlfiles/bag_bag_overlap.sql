0131b6d352bdf62f8fe9947fd639a567661fc987
GinaStavropoulou <ginastavropoulou@gmail.com>
2026-07-30 11:29:17 +0200

    added array of overllapping buildings

--- packages/core/src/bag3d/core/sqlfiles/bag_bag_overlap.sql
+++ packages/core/src/bag3d/core/sqlfiles/bag_bag_overlap.sql
@@ -1,22 +1,27 @@
 DROP TABLE IF EXISTS ${new_table} CASCADE;
 
 CREATE TABLE ${new_table} AS
-WITH isect AS (SELECT bag1.fid
-                    , ST_Intersection(bag1.geometrie, bag2.geometrie) isection
-               FROM ${bag_pandactueelbestaand} bag1
-                        JOIN
-                    ${bag_pandactueelbestaand} bag2
-                    ON ST_Intersects(bag1.geometrie, bag2.geometrie)
-               WHERE bag1.fid != bag2.fid)
-   , dissolve AS (SELECT fid
-                       , st_area(st_unaryunion(st_collect(isection))) overlap_area
-                  FROM isect
-                  GROUP BY fid)
-SELECT d.fid
-     , CASE
-           WHEN overlap_area IS NULL OR overlap_area < 1.0
-               THEN 0.0
-           ELSE overlap_area
-    END AS b3_bag_bag_overlap
-FROM dissolve d
-         JOIN ${bag_pandactueelbestaand} b ON d.fid = b.fid;
+WITH isect AS (
+    SELECT bag1.fid
+         , ST_Intersection(bag1.geometrie, bag2.geometrie) isection
+         , bag2.identificatie AS overlapping_identificatie
+    FROM ${bag_pandactueelbestaand} bag1
+    JOIN ${bag_pandactueelbestaand} bag2
+        ON ST_Intersects(bag1.geometrie, bag2.geometrie)
+    WHERE bag1.fid != bag2.fid
+)
+, dissolve AS (
+    SELECT fid
+         , st_area(st_unaryunion(st_collect(isection))) overlap_area
+         , ARRAY_AGG(DISTINCT overlapping_identificatie ORDER BY overlapping_identificatie) AS overlapping_ids
+    FROM isect
+    GROUP BY fid
+)
+SELECT b.fid
+     , COALESCE(
+           CASE WHEN d.overlap_area < 1.0 THEN 0.0 ELSE d.overlap_area END,
+           0.0
+       ) AS b3_bag_bag_overlap
+     , COALESCE(d.overlapping_ids, ARRAY[]::text[]) AS b3_bag_bag_overlap_ids
+FROM ${bag_pandactueelbestaand} b
+LEFT JOIN dissolve d ON b.fid = d.fid;
