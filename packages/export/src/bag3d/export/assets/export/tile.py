7cd86e55cde64d2551975448d98909436dd5635a
Balázs Dukai <balazs.dukai@gmail.com>
2026-07-31 18:31:52 +0200

    Update tyler assets to tyler v1

--- packages/export/src/bag3d/export/assets/export/tile.py
+++ packages/export/src/bag3d/export/assets/export/tile.py
@@ -1,3 +1,5 @@
+from enum import StrEnum
+
 import json
 import os
 from os import getenv
@@ -15,6 +17,14 @@ from bag3d.common.resources.files import FileStoreResource
 from bag3d.common.resources.version import ReleaseVersionResource
 
 logger = get_dagster_logger("export.tile")
+
+
+class TylerOutputFormat(StrEnum):
+    CESIUM3DTILES = "3dtiles"
+    GPKG = "gpkg"
+    CITYJSON = "cityjson"
+    OBJ = "obj"
+    TSV = "tsv"
 
 
 def create_sequence_header_file(template_file, output_file, version_3dbag):
@@ -35,7 +45,7 @@ def create_sequence_header_file(template_file, output_file, version_3dbag):
 
 def generate_tyler_config(
     specs: Specs3DBAGResource,
-    data_format: str,
+    data_format: TylerOutputFormat,
     locations: Union[
         tuple[CityJSONLocation], tuple[GpkgLocation], tuple[Cesium3dTilesLocation]
     ],
@@ -51,9 +61,12 @@ def generate_tyler_config(
     Raises:
         ValueError: With `format=='cesium3dtiles'` if `if len(locations) > 1` or `if not isinstance(location, Cesium3dTilesLocation)`.
     """
-    cli_params = []
+    cli_params = [
+        f"--format={data_format}",
+        "--qtree-capacity=280000",
+    ]
     output_dir = None
-    if data_format == "cesium3dtiles":
+    if data_format == TylerOutputFormat.CESIUM3DTILES:
         if len(locations) > 1:
             raise ValueError(
                 "Can only generate tyler config for cesium3dtiles for one location at a time, because the location contains the Level of Detail and we produce a separate tileset per LoD."
@@ -68,40 +81,52 @@ def generate_tyler_config(
             [
                 f"--output={output_dir}",
                 "--3dtiles-metadata-class=building",
-                "--qtree-capacity=280000",
                 "--grid-minz=-50",
                 "--grid-maxz=400",
-                "--object-type=Building",
-                "--object-type=BuildingPart",
                 f"--lod-building-part={location.lod}",
-                f"--lod-building={location.lod}",
+                "--object-type=BuildingPart",
+                "--include-parent-attributes",
             ]
         )
         attributes = specs.applies_to(data_format=data_format, locations=locations)
         for a_name, a_spec in attributes:
             cli_params.append(f"--object-attribute={a_name}:{a_spec.type.as_geof()}")
-    elif data_format == "multi":
+    elif data_format == TylerOutputFormat.GPKG:
         output_dir = export_dir
         cli_params.extend(
             [
                 f"--output={output_dir}",
-                "--format=multi",
-                "--object-type=Building",
-                "--object-type=BuildingPart",
-                "--qtree-capacity=280000",
                 "--grid-export",
+                "--gpkg-split-lod",
+                "--gpkg-include-semantics",
+                "--gpkg-include-hierarchy",
+            ]
+        )
+    elif data_format == TylerOutputFormat.CITYJSON:
+        output_dir = export_dir
+        cli_params.extend(
+            [
+                f"--output={output_dir}",
+            ]
+        )
+    elif data_format == TylerOutputFormat.OBJ:
+        output_dir = export_dir
+        cli_params.extend(
+            [
+                f"--output={output_dir}",
+                "--object-type=BuildingPart",
             ]
         )
     else:
         raise ValueError(
-            f"data_format must be one of 'multi', 'cesium3dtiles', got {data_format}"
+            f"data_format must be one of 'cityjson', 'gpkg', 'obj', 'cesium3dtiles', got {data_format}"
         )
     output_dir.mkdir(parents=True, exist_ok=True)
     return cli_params, output_dir
 
 
 def reconstruction_output_tiles_func(
-    data_format: str,
+    data_format: TylerOutputFormat,
     file_store: FileStoreResource,
     version: ReleaseVersionResource,
     geoflow: GeoflowResource,
@@ -124,30 +149,21 @@ def reconstruction_output_tiles_func(
         os.getenv("TYLER_METADATA_JSON"), sequence_header_file, version_3dbag
     )
     num_threads = kwargs["rayon_num_threads"]
-    cmd = [
-        f"RAYON_NUM_THREADS={num_threads}",
-        f"RUST_LOG={'debug' if kwargs.get('verbose', False) else 'info'}",
-        f"TYLER_RESOURCES_DIR={os.getenv('TYLER_RESOURCES_DIR')}",
-        "{exe}",
-        str(reconstructed_root_dir),
-        "--exe-geof",
-        str(geoflow.runner.exes["geof"]),
-    ]
-    if data_format == "multi":
-        exe_name = "tyler-multiformat"
-    elif data_format == "cesium3dtiles":
-        exe_name = "tyler"
-    else:
-        raise ValueError(
-            f"invalid data_format: {data_format}, only 'multi' and 'cesium3dtiles' are allowed"
-        )
+    exe_name = "tyler"
     cli_params, output_dir = generate_tyler_config(
         specs=specs,
         data_format=data_format,
         locations=kwargs["locations"],
         export_dir=export_dir,
     )
+    cmd = [
+        f"RAYON_NUM_THREADS={num_threads}",
+        f"RUST_LOG={'debug' if kwargs.get('verbose', False) else 'info'}",
+        "{exe}",
+    ]
     cmd.extend(cli_params)
+    # Append the input directory path as last
+    cmd.append(str(reconstructed_root_dir))
     logger.debug(" ".join(cmd))
     tyler.runner.run(
         " ".join(cmd),
@@ -168,10 +184,10 @@ class TylerConfig(Config):
 
 @asset(
     deps={AssetKey(("floors_estimation", "save_cjfiles"))},
-    code_version=tool_versions.get_version("tyler-multiformat"),
+    code_version=tool_versions.get_version("tyler"),
     pool="tyler",
 )
-def reconstruction_output_multitiles(
+def reconstruction_output_cityjson(
     config: TylerConfig,
     metadata,
     tyler: TylerResource,
@@ -180,13 +196,79 @@ def reconstruction_output_multitiles(
     version: ReleaseVersionResource,
     specs: Specs3DBAGResource,
 ) -> Path:
-    """Tiles for distribution, in CityJSON, OBJ, GPKG formats.
+    """Tiles for distribution in CityJSON format.
     Generated with tyler."""
     with metadata.open("r") as fo:
         metadata_lineage = json.load(fo)
     version_3dbag = metadata_lineage["identificationInfo"]["citation"]["edition"]
     return reconstruction_output_tiles_func(
-        data_format="multi",
+        data_format=TylerOutputFormat.CITYJSON,
+        file_store=file_store,
+        version=version,
+        geoflow=geoflow,
+        specs=specs,
+        tyler=tyler,
+        version_3dbag=version_3dbag,
+        rayon_num_threads=config.concurrency,
+        locations=tuple(),
+        verbose=config.verbose,
+    )
+
+
+@asset(
+    deps={AssetKey(("floors_estimation", "save_cjfiles"))},
+    code_version=tool_versions.get_version("tyler"),
+    pool="tyler",
+)
+def reconstruction_output_gpkg(
+    config: TylerConfig,
+    metadata,
+    tyler: TylerResource,
+    geoflow: GeoflowResource,
+    file_store: FileStoreResource,
+    version: ReleaseVersionResource,
+    specs: Specs3DBAGResource,
+) -> Path:
+    """Tiles for distribution in GPKG format.
+    Generated with tyler."""
+    with metadata.open("r") as fo:
+        metadata_lineage = json.load(fo)
+    version_3dbag = metadata_lineage["identificationInfo"]["citation"]["edition"]
+    return reconstruction_output_tiles_func(
+        data_format=TylerOutputFormat.GPKG,
+        file_store=file_store,
+        version=version,
+        geoflow=geoflow,
+        specs=specs,
+        tyler=tyler,
+        version_3dbag=version_3dbag,
+        rayon_num_threads=config.concurrency,
+        locations=tuple(),
+        verbose=config.verbose,
+    )
+
+
+@asset(
+    deps={AssetKey(("floors_estimation", "save_cjfiles"))},
+    code_version=tool_versions.get_version("tyler"),
+    pool="tyler",
+)
+def reconstruction_output_obj(
+    config: TylerConfig,
+    metadata,
+    tyler: TylerResource,
+    geoflow: GeoflowResource,
+    file_store: FileStoreResource,
+    version: ReleaseVersionResource,
+    specs: Specs3DBAGResource,
+) -> Path:
+    """Tiles for distribution OBJ format.
+    Generated with tyler."""
+    with metadata.open("r") as fo:
+        metadata_lineage = json.load(fo)
+    version_3dbag = metadata_lineage["identificationInfo"]["citation"]["edition"]
+    return reconstruction_output_tiles_func(
+        data_format=TylerOutputFormat.OBJ,
         file_store=file_store,
         version=version,
         geoflow=geoflow,
@@ -219,7 +301,7 @@ def reconstruction_output_3dtiles_lod12(
         metadata_lineage = json.load(fo)
     version_3dbag = metadata_lineage["identificationInfo"]["citation"]["edition"]
     return reconstruction_output_tiles_func(
-        data_format="cesium3dtiles",
+        data_format=TylerOutputFormat.CESIUM3DTILES,
         file_store=file_store,
         version=version,
         geoflow=geoflow,
@@ -252,7 +334,7 @@ def reconstruction_output_3dtiles_lod13(
         metadata_lineage = json.load(fo)
     version_3dbag = metadata_lineage["identificationInfo"]["citation"]["edition"]
     return reconstruction_output_tiles_func(
-        data_format="cesium3dtiles",
+        data_format=TylerOutputFormat.CESIUM3DTILES,
         file_store=file_store,
         version=version,
         geoflow=geoflow,
@@ -285,7 +367,7 @@ def reconstruction_output_3dtiles_lod22(
         metadata_lineage = json.load(fo)
     version_3dbag = metadata_lineage["identificationInfo"]["citation"]["edition"]
     return reconstruction_output_tiles_func(
-        data_format="cesium3dtiles",
+        data_format=TylerOutputFormat.CESIUM3DTILES,
         file_store=file_store,
         version=version,
         geoflow=geoflow,
