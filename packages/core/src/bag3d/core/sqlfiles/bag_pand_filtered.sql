51b09c524d1926c7fab8ef5af2785017c5786262
GinaStavropoulou <ginastavropoulou@gmail.com>
2026-08-12 17:12:44 +0200

    also filtered table

--- packages/core/src/bag3d/core/sqlfiles/bag_pand_filtered.sql
+++ packages/core/src/bag3d/core/sqlfiles/bag_pand_filtered.sql
@@ -7,12 +7,14 @@ FROM ${bag_pand} bag
 LEFT JOIN ${bag_bgt_join} bbj ON bag.identificatie = bbj.identificatie
 LEFT JOIN ${bag_bag_overlap} bbo ON bag.fid = bbo.fid
 WHERE NOT (
-    (bbj.identificatie IS NULL AND st_area(bag.geometrie) > 1000)
+    (bbj.relatievehoogteligging < 0)
+    OR
+    (bbj.identificatie IS NULL
+     AND bbo.b3_bag_bag_overlap / st_area(bag.geometrie) > 0.1
+     AND cardinality(bbo.b3_bag_bag_overlap_ids) > 1)
     OR
     (bbj.identificatie IS NOT NULL
      AND st_area(bbj.bgt_geometrie) / st_area(bbj.bag_geometrie) < 0.1)
-    OR
-    (bbo.b3_bag_bag_overlap / st_area(bag.geometrie) > 0.1)
 );
 
 CREATE TABLE ${bag_pand_removed} AS
