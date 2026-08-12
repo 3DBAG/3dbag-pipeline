7f44d34a8bff1cacbf205a947c91b77e9b2f7f15
GinaStavropoulou <ginastavropoulou@gmail.com>
2026-08-12 17:07:20 +0200

    fixed bag_bgt join and filter

--- packages/core/src/bag3d/core/sqlfiles/bag_bgt_join.sql
+++ packages/core/src/bag3d/core/sqlfiles/bag_bgt_join.sql
@@ -1,19 +1,49 @@
 DROP TABLE IF EXISTS ${new_table} CASCADE;
 
 CREATE TABLE ${new_table} AS
-WITH filtered AS (
+WITH
+overground_bgt AS (
+    SELECT *
+    FROM ${bgt_pand} bt
+    WHERE bt.relatievehoogteligging > -1
+),
+grouped_overground_bgt AS (
     SELECT
-        bag.identificatie,
-        bag.geometrie AS bag_geometrie,
-        bt.geometrie AS bgt_geometrie
-    FROM ${bag_pand} bag
-    JOIN ${bgt_pand} bt
-        ON bt.identificatiebagpnd = SUBSTRING(bag.identificatie FROM 15)
-        AND bag.geometrie && bt.geometrie
+        bt.identificatiebagpnd,
+        ST_UnaryUnion(ST_Collect(bt.geometrie)) AS geometrie,
+        0 AS relatievehoogteligging
+    FROM overground_bgt bt
+    GROUP BY bt.identificatiebagpnd
+),
+only_underground_bgt AS (
+    SELECT
+        bt.identificatiebagpnd,
+        bt.geometrie,
+        bt.relatievehoogteligging
+    FROM ${bgt_pand} bt
+    LEFT JOIN grouped_overground_bgt fb
+        ON bt.identificatiebagpnd = fb.identificatiebagpnd
+    WHERE fb.identificatiebagpnd IS NULL
+),
+grouped_only_underground_bgt AS (
+    SELECT
+        obt.identificatiebagpnd,
+        ST_UnaryUnion(ST_Collect(obt.geometrie)) AS geometrie,
+        obt.relatievehoogteligging
+    FROM only_underground_bgt obt
+    GROUP BY obt.identificatiebagpnd, obt.relatievehoogteligging
+),
+grouped_bgt AS (
+    SELECT * FROM grouped_overground_bgt
+    UNION
+    SELECT * FROM grouped_only_underground_bgt
 )
 SELECT
-    identificatie,
-    bag_geometrie,
-    ST_UnaryUnion(ST_Collect(bgt_geometrie)) AS bgt_geometrie
-FROM filtered
-GROUP BY identificatie, bag_geometrie;
+    bag.identificatie,
+    bag.geometrie AS bag_geometrie,
+    gbt.geometrie AS bgt_geometrie,
+    gbt.relatievehoogteligging
+FROM ${bag_pand} bag
+JOIN grouped_bgt gbt
+    ON gbt.identificatiebagpnd = SUBSTRING(bag.identificatie FROM 15)
+    AND bag.geometrie && gbt.geometrie
--- packages/core/src/bag3d/core/sqlfiles/bag_pand_filtered.sql
+++ packages/core/src/bag3d/core/sqlfiles/bag_pand_filtered.sql
@@ -16,23 +16,34 @@ WHERE NOT (
 );
 
 CREATE TABLE ${bag_pand_removed} AS
-SELECT bag.*
-     , ARRAY_REMOVE(ARRAY[
-           CASE WHEN bbj.identificatie IS NULL AND st_area(bag.geometrie) > 1000
-                THEN 'no_bgt_match_area_gt_1000' END,
-           CASE WHEN bbj.identificatie IS NOT NULL
-                     AND st_area(bbj.bgt_geometrie) / st_area(bbj.bag_geometrie) < 0.1
-                THEN 'bgt_bag_ratio_lt_0.1' END,
-           CASE WHEN bbo.b3_bag_bag_overlap / st_area(bag.geometrie) > 0.1
-                THEN 'bag_bag_overlap_gt_0.1' END
-       ], NULL) AS removal_reasons
+SELECT
+    bag.identificatie,
+    bag.geometrie,
+    bbj.bgt_geometrie,
+    bag.status,
+    ARRAY_REMOVE(
+        ARRAY[
+            CASE WHEN bbj.relatievehoogteligging < 0
+                THEN 'BGT relatieve hoogteligging < 0' END,
+            CASE WHEN bbj.identificatie IS NULL
+                AND bbo.b3_bag_bag_overlap / st_area(bag.geometrie) > 0.1
+                AND cardinality(bbo.b3_bag_bag_overlap_ids) > 1
+                THEN 'No BGT match and overlap with more than 1 building' END,
+            CASE WHEN bbj.identificatie IS NOT NULL
+                AND st_area(bbj.bgt_geometrie) / st_area(bbj.bag_geometrie) < 0.1
+                THEN 'BGT-BAG ratio > 0.1' END
+        ],
+        NULL
+    ) AS removal_reasons
 FROM ${bag_pand} bag
 LEFT JOIN ${bag_bgt_join} bbj ON bag.identificatie = bbj.identificatie
-LEFT JOIN ${bag_bag_overlap} bbo ON bag.fid = bbo.fid
+JOIN ${bag_bag_overlap} bbo ON bag.fid = bbo.fid
 WHERE
-    (bbj.identificatie IS NULL AND st_area(bag.geometrie) > 1000)
+    (bbj.relatievehoogteligging < 0)
+    OR
+    (bbj.identificatie IS NULL
+     AND bbo.b3_bag_bag_overlap / st_area(bag.geometrie) > 0.1
+     AND cardinality(bbo.b3_bag_bag_overlap_ids) > 1)
     OR
     (bbj.identificatie IS NOT NULL
-     AND st_area(bbj.bgt_geometrie) / st_area(bbj.bag_geometrie) < 0.1)
-    OR
-    (bbo.b3_bag_bag_overlap / st_area(bag.geometrie) > 0.1);
+     AND st_area(bbj.bgt_geometrie) / st_area(bbj.bag_geometrie) < 0.1);
